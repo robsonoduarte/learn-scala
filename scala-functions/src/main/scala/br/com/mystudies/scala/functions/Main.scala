d829a2d158dba54a4a9b39d91b8a7011d1907944
robsonoduarte <robson.o.d@gmail.com>
2013-11-07 18:52:20 -0200

    starting studies about functions in scala

--- scala-functions/src/main/scala/br/com/mystudies/scala/functions/Main.scala
+++ scala-functions/src/main/scala/br/com/mystudies/scala/functions/Main.scala
@@ -1,10 +1,38 @@
 package br.com.mystudies.scala.functions
 
+import scala.collection.immutable.Range
+import scala.collection.mutable.LinkedList
+import scala.collection.mutable.AbstractBuffer
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+
 object Main extends App {
 
-  def values(func: Int => Int){
+  
+  
+  def values(func: Int => Int, low: Int, high: Int){
+	  
+	  var x = new HashMap[Int, Int]
+	  
+	  for( i <- low to high ){	   
+		  x +=  i -> func(high)
+	  }
     
+	  print(x)
+   
   }
   
-  values(x => x * x)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  values(x => x * x  , -5, 5)
+
+  
 }
