7e11d14600f33113c67eea58ecdcf26cf49ab85f
Robson Duarte <robson.o.d@gmail.com>
2015-07-08 21:32:35 -0300

    writing example of function with receive another function and receive one sequence than apply received function in sequence functions.

--- scala-functions/src/main/scala/br/com/mystudies/scala/higher/order/functions/UsefulHigherOrderFunctions.scala
+++ scala-functions/src/main/scala/br/com/mystudies/scala/higher/order/functions/UsefulHigherOrderFunctions.scala
@@ -72,4 +72,15 @@ object UsefulHigherOrderFunctions extends App {
   println(f(6))
 
 
+
+
+
+  def largest(fun: (Int) => Int, inputs: Seq[Int]) = inputs map(fun) max
+
+
+  println(largest(x => 10 * x - x * x,  1 to 10))
+
+
+
+
 }
