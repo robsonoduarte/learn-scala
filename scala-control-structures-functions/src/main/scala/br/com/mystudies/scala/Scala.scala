ca2e367f0fcd06c783b0ae1679f1e48df8c0cc96
robsonoduarte <robson.o.d@gmail.com>
2015-10-28 13:26:22 -0200

    resolve compilation problems.

--- scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Scala.scala
+++ scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Scala.scala
@@ -3,13 +3,13 @@ package br.com.mystudies.scala
 object Scala {
 
 	def containsUpperCase(s: String) = {
-		s.toCharArray.exists( (c: Char) => c.isUpperCase )
+	 	s.toCharArray.exists( (c: Char) => c.isUpper )
 	}
 
 
   	def main(args: Array[String]) {
-		println(containsUpperCase("Scala"))
-		println(containsUpperCase("scala"))
+		  println(containsUpperCase("Scala"))
+		  println(containsUpperCase("scala"))
   	}
 
 }
