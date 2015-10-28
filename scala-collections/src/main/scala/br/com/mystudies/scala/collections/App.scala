bd0c115ccd1390d0f9c4ba2ba9f44648d4a4e7a1
robsonoduarte <robson.o.d@gmail.com>
2015-10-28 20:00:47 -0200

    remove the scala class of functions examples and put the starts class example of Java 8 and Scala collections.

---
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/App.scala
@@ -0,0 +1,5 @@
+package br.com.mystudies.scala.collections
+
+object App extends App { 
+  List(1, 2, 3, 4 ) foreach { println _}
+}
---
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/Main.java
@@ -0,0 +1,9 @@
+package br.com.mystudies.scala.collections;
+
+import static java.util.Arrays.asList;
+
+public class Main {
+	public static void main(String[] args) {		
+		asList(1, 2, 3, 4).forEach(System.out::println);		
+	}
+}
