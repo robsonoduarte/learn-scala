e5ed9b30c9551a1ff909aef785f4e9361a5b1c82
robsonoduarte <robson.o.d@gmail.com>
2015-10-29 23:24:38 -0200

    starting the exercises of scala's collections.

--- scala-collections/src/main/scala/br/com/mystudies/scala/collections/App.scala
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/App.scala
@@ -1,5 +1,33 @@
 package br.com.mystudies.scala.collections
 
+import java.util.Hashtable
+import scala.collection.mutable.HashTable
+import scala.collection.immutable.HashMap
+
 object App extends App { 
   List(1, 2, 3, 4 ) foreach { println _}
+  
+  
+ 
+  
+  def fun(world : String ){
+    
+    var map = HashMap.empty[Int, Char]
+    
+    
+    world foreach { c => map += ( world.indexOf(c) -> c ) }  
+    
+    map
+    
+  }
+  
+  
+  
+  
+   print(fun("Robson"))
+  
+  
+  
 }
+
+  
