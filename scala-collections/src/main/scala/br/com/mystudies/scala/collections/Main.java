afcf7d69276fd0a35a94acaeb6cbed15207a8324
robsonoduarte <robson.o.d@gmail.com>
2015-11-18 22:08:09 -0200

    example of streams in java to compare with scala.

--- scala-collections/src/main/scala/br/com/mystudies/scala/collections/Main.java
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/Main.java
@@ -1,9 +1,24 @@
 package br.com.mystudies.scala.collections;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {		
-		asList(1, 2, 3, 4).forEach(System.out::println);		
+	
+		
+	public static List<Integer> removeZeros(List<Integer> list){
+		return list.stream()
+				.filter(x  -> x != 0)
+				.collect(toList());				
 	}
+	
+	
+	
+	public static void main(String[] args) {		
+		System.out.println(removeZeros(asList(1, 2, 3, 4)));		
+	}
+	
+	
 }
