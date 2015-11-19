e95aede8dfc39f6ab585ac1d48f543230d25af93
robsonoduarte <robson.o.d@gmail.com>
2015-11-18 22:12:58 -0200

    new example.

--- scala-collections/src/main/scala/br/com/mystudies/scala/collections/App2.scala
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/App2.scala
@@ -2,22 +2,49 @@ package br.com.mystudies.scala.collections
 
 import scala.collection.immutable.HashMap
 import scala.collection.immutable.HashSet
+import scala.collection.immutable.Map
 
 
 
 
 object App2 extends App {
-     
+   
+  
+  
+  
+  
+  
+  
+  def func(list: List[String], map : Map[String, Int]) = {
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+    // ------------------------------------------------------------------------------------
+  
+  
    def removeZeroes(list : List[Int] ) = {
        list filterNot(_ == 0)
    }
-   
    
    println(removeZeroes(List(1, 2, 0, 3, 0)))
    
   
    
-     
+   // ------------------------------------------------------------------------------------
+   
    
    def indexes(world : String ) = {
 		   var map =  HashMap.empty[Char, HashSet[Int]];     
@@ -27,10 +54,9 @@ object App2 extends App {
 		   map
    }
    
-   
-   
-   
+  
    println(indexes("ROBSON OLIVEIRA DUARTE"))
+   
 }
 
   
