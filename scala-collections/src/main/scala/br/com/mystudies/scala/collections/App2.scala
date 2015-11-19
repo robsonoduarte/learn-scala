7395557353d076d4b0507027238f195297b032bb
robsonoduarte <robson.o.d@gmail.com>
2015-11-18 22:08:39 -0200

    add simple example of stream.

--- scala-collections/src/main/scala/br/com/mystudies/scala/collections/App2.scala
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/App2.scala
@@ -7,23 +7,30 @@ import scala.collection.immutable.HashSet
 
 
 object App2 extends App {
-  
-
-  
-  
-  
-  def indexes(world : String ) = {
-     var map =  HashMap.empty[Char, HashSet[Int]];     
-     world foreach( c => {
-         map = map + ( c -> HashSet.empty.+(  world.indexOf(c))) 
-     })     
-     map
-  }
-    
-  
-   print(indexes("ROBSON OLIVEIRA DUARTE"))
+     
+   def removeZeroes(list : List[Int] ) = {
+       list filterNot(_ == 0)
+   }
+   
+   
+   println(removeZeroes(List(1, 2, 0, 3, 0)))
    
   
+   
+     
+   
+   def indexes(world : String ) = {
+		   var map =  HashMap.empty[Char, HashSet[Int]];     
+		   world foreach( c => {
+			   map = map + ( c -> HashSet.empty.+(  world.indexOf(c))) 
+		   })     
+		   map
+   }
+   
+   
+   
+   
+   println(indexes("ROBSON OLIVEIRA DUARTE"))
 }
 
   
