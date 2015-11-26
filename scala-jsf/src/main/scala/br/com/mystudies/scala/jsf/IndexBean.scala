29a20c55d06c26618576e9801460135d6986b126
robsonoduarte <robson.o.d@gmail.com>
2015-11-25 22:02:03 -0200

    remove app class and add IndexBean ( scala )

---
+++ scala-jsf/src/main/scala/br/com/mystudies/scala/jsf/IndexBean.scala
@@ -0,0 +1,30 @@
+package br.com.mystudies.scala.jsf
+
+import javax.faces.bean.ManagedBean
+import scala.beans.BeanProperty
+
+@ManagedBean
+class IndexBean {
+  
+  
+  @BeanProperty
+  var name = "Robson Duarte";
+  
+  
+  
+  @BeanProperty
+  var message = "Hello !";
+  
+  
+  
+  def navigate() = { "navigate" }
+  
+  
+  
+  def action(){ 
+     name = "Duarte"
+  }
+  
+  
+  
+}
