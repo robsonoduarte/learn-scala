f810fb7eae605543ff01ba3a5183a251adec302f
Robson Duarte <robson.o.d@gmail.com>
2016-02-16 23:13:48 -0200

    Exercise 7

---
+++ scala-collections/src/main/scala/br/com/mystudies/scala/collections/Ex7.scala
@@ -0,0 +1,15 @@
+package br.com.mystudies.scala.collections
+
+object Ex7 extends App {
+
+
+  val prices = List(5.0, 20.0, 9.95 )
+  val quantities = List( 10, 2, 1 )
+
+
+   println( (prices zip quantities))
+   println( (prices zip quantities) map {p => p._1 * p._2})
+   println( ((prices zip quantities) map {p => p._1 * p._2}) sum)
+
+
+}
