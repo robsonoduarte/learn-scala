a6098283b04f9f6ea27f499cd0f14312ff157b4a
Robson Duarte <robson.o.d@gmail.com>
2016-02-19 20:44:13 -0200

    starting example

--- pragmatic-scala/scala-exploring/src/main/scala/br/com/mystudies/scala/collections/TopStock.scala
+++ pragmatic-scala/scala-exploring/src/main/scala/br/com/mystudies/scala/collections/TopStock.scala
@@ -2,10 +2,17 @@ package br.com.mystudies.scala.collections
 
 object TopStock extends App {
 
+  val simboly = List();
+
+  val year = 2014;
 
 
 
 
+
+
+  def getYearEndClosingPrice(symbol: String, year: Int ) = {
+  }
 
 
 }
