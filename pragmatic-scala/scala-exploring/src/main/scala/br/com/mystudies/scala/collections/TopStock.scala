d16bf12ebfe6632a41a13ebb6478c794212d817b
Robson Duarte <robson.o.d@gmail.com>
2016-02-22 21:20:08 -0300

    finished the first example.

--- pragmatic-scala/scala-exploring/src/main/scala/br/com/mystudies/scala/collections/TopStock.scala
+++ pragmatic-scala/scala-exploring/src/main/scala/br/com/mystudies/scala/collections/TopStock.scala
@@ -2,17 +2,32 @@ package br.com.mystudies.scala.collections
 
 object TopStock extends App {
 
-  val simboly = List();
-
+  val simbols = List("AMD", "AAPL", "AMZN", "IBM", "ORCL", "MSFT");
   val year = 2014;
 
 
-
-
+  val (topStock, topPrice) = simbols map{ ticker => (ticker, getYearEndClosingPrice(ticker, year))} maxBy{ stockPrice => stockPrice._2}
 
 
   def getYearEndClosingPrice(symbol: String, year: Int ) = {
+    var url = s"http://ichart.finance.yahoo.com/table.csv?s=" + s"$symbol&a=11&b=01&c$year&d=11&e31&f=$year&g=m"
+
+
+    val data = io.Source.fromURL(url).mkString
+    val price = data.split("\n")(1).split(",")(3).toDouble
+
+
+    price
+
   }
 
+
+
+
+
+
+
+
+  printf(s"Top stock of $year is $topStock closing at price $$$topPrice")
 
 }
