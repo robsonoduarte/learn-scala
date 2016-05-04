4c442aa24dc02fdde9076345b0e8898246bed24f
Robson Duarte <robson.o.d@gmail.com>
2016-05-03 22:15:53 -0300

    add example using the until method ( RichInt ) and simple example of short sintaxe without dot and parentheses.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Greetings.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Greetings.scala
@@ -6,12 +6,21 @@ object Greetings extends App {
     print(s"$i,")
   }
 
-
   println("Scala Rocks!!!")
 
 
 
 
+  for(i <- 1 until 3){
+	  print(s"$i,")
+  }
+
+  println("Scala Rocks!!!")
+
+
+  //using short form when the method take zero or one parameter
+  println(1.until(3))
+  println(1 until 3)
 
 
 
