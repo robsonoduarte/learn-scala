b7e987ed48e305583a6312e1e0ae688389b20ec2
Robson Duarte <robson.o.d@gmail.com>
2016-05-03 22:08:01 -0300

    rename package.

---
+++ pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Greetings.java
@@ -0,0 +1,13 @@
+package br.com.mystudies.from.java;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 4; i++) {
+			System.out.print(i + ",");
+		}
+
+		System.out.println("Scala Rocks !!!");
+	}
+}
--- pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/to/scala/Greetings.java
+++
@@ -1,8 +0,0 @@
-package br.com.mystudies.from.java.to.scala;
-
-public class Greetings {
-
-	public static void main(String[] args) {
-
-	}
-}
