5d180e54c375a31ea0dfeac88c26f925d61b3fca
Robson Duarte <robson.o.d@gmail.com>
2016-05-09 19:24:03 -0300

    add example of Scala class for java primitive.

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/ScalaInt.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.from.scala
+
+object ScalaInt {
+
+
+  // example of Scala classe for java primitives
+  val capacity : Int = 10
+  val list = new java.util.ArrayList[String]
+
+  list.ensureCapacity(capacity) //public void ensureCapacity(int minCapacity)
+
+
+}
