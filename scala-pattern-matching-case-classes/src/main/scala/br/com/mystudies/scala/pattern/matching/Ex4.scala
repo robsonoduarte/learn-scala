79eca720f2aca7b87a5fd0558c67b4290df46399
Robson Duarte <robson.o.d@gmail.com>
2016-05-10 22:13:21 -0300

    continue the ex 4.

--- scala-pattern-matching-case-classes/src/main/scala/br/com/mystudies/scala/pattern/matching/Ex4.scala
+++ scala-pattern-matching-case-classes/src/main/scala/br/com/mystudies/scala/pattern/matching/Ex4.scala
@@ -1,10 +1,34 @@
 package br.com.mystudies.scala.pattern.matching
 
-abstract class Item(){}
+abstract class Item
 
 
-case class Product() extends Item{}
-case class Multiple() extends Item{}
+case class Product(description: String, price: Double) extends Item
+case class Bundle(description: String, discount: Double, items: Item*) extends Item
+case class Multiple(count: Item, item: Item) extends Item
 
+
+
+object test extends App{
+
+
+  def price(it: Item): Double = it match {
+    case Product(_, p) => p
+    case Bundle(_,disc,its @_*) => its.map(price _).sum - disc
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
