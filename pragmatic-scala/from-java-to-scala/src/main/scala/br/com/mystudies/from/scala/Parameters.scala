6efc81f15c71e8f277a8803a93cb02a1f8347a2f
Robson Duarte <robson.o.d@gmail.com>
2016-05-12 23:03:39 -0300

    add example about explode notation.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Parameters.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Parameters.scala
@@ -4,13 +4,23 @@ package br.com.mystudies.from.scala
 
 object Parameters extends App {
 
-    def max (values: Int*) = values.foldLeft(values(0))(Math.max) // -> ((a,b) => Math.max(a,b))
+    def max (values: Int*) = println(values.foldLeft(values(0))(Math.max)) // -> ((a,b) => Math.max(a,b))
 
-    println(max(11,3,10))
-    println(max(0,3,10, 20, -1, 4))
+    max(11,3,10)
+    max(0,3,10, 20, -1, 4)
 
 
     def function(input: Int*) = println(input.getClass)
 
     function(1,2,3,4,5)
+
+
+
+    val numbers = Array(1,2,3,4,5)
+    //max(numbers) -> the error of type incompatibility the parameter is Int not Array
+
+    // but we can passa the array using the explode notation
+    max(numbers: _*)
+
+
 }
