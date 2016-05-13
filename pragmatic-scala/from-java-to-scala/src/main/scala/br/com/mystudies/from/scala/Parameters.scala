99e57531b784f01c1d44c865dc44a2ed1238dcae
Robson Duarte <robson.o.d@gmail.com>
2016-05-12 22:46:52 -0300

    put one example to see how type of parameter Scala define.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Parameters.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Parameters.scala
@@ -6,5 +6,11 @@ object Parameters extends App {
 
     def max (values: Int*) = values.foldLeft(values(0))(Math.max) // -> ((a,b) => Math.max(a,b))
 
-    print(max(11,3,10))
+    println(max(11,3,10))
+    println(max(0,3,10, 20, -1, 4))
+
+
+    def function(input: Int*) = println(input.getClass)
+
+    function(1,2,3,4,5)
 }
