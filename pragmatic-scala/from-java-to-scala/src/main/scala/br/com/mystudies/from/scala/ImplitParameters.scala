d908202b3f014d9da833ec0f290c9190258a132a
Robso Duarte <robson.o.d@gmail.com>
2016-07-07 21:50:34 -0300

    add example how use implicit

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/ImplitParameters.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/ImplitParameters.scala
@@ -3,12 +3,27 @@ package br.com.mystudies.from.scala
 
 object ImplicitParameters extends App{
 
-  def connectToNetwork(user: String)(implicit wifi: Wifi){}
+  def connectToNetwork(user: String)(implicit wifi: Wifi) = println(s"User: $user connected to  WIFI $wifi")
 
 
+  def atOffice = {
+    println("--- at the office ---")
+    implicit def officeNetwork = new Wifi("office-network")
+    val cafeteriaNetwork = new Wifi("cafe-connect")
+
+    connectToNetwork("Robson")(cafeteriaNetwork)
+    connectToNetwork("Duarte")
+    connectToNetwork("Oliveira")
+  }
+
+
+
+  atOffice
 
 
 }
+
+
 
 
 
