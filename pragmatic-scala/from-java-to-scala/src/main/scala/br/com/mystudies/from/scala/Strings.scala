21d35c768aa23494e0044b3397ad8954909ed740
Robso Duarte <robson.o.d@gmail.com>
2016-07-14 21:11:48 -0300

    add example about String in Scala ( raw string )

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Strings.scala
@@ -0,0 +1,12 @@
+package br.com.mystudies.from.scala
+
+object String extends App{
+
+
+  val str = """Scala is an acronym for “Scalable Language”.This means that Scala grows with you.
+               You can play with it by typing one-line expressions and observing the results.
+               But you can also rely on it for large mission critical systems, as many companies, including Twitter, LinkedIn, or Intel do."""
+
+  print(str)
+
+}
