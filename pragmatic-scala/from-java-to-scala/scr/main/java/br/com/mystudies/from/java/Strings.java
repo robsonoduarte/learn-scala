0b1227784b115e3907c76397f1464a65bb92e455
Robso Duarte <robson.o.d@gmail.com>
2016-07-14 21:13:08 -0300

    add simple example o string multiple lines in Java to compare with scala.

---
+++ pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Strings.java
@@ -0,0 +1,14 @@
+package br.com.mystudies.from.java;
+
+public class Strings {
+
+	public static void main(String[] args) {
+
+		String str = "\"Java language\" redirects here. "
+				+ "\nFor the natural language from the Indonesian island of Java, see Javanese language."
+				+ "\nThis article is about a programming language. For the software package downloaded from java.com, "
+				+ "\nsee Java SE.Not to be confused with JavaScript.";
+
+		System.out.println(str);
+	}
+}
