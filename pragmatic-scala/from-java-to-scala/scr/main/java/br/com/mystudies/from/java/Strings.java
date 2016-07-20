b197d4f4dc26a84106dd9d6d790b58d71ab8d9ab
Robso Duarte <robson.o.d@gmail.com>
2016-07-19 22:03:19 -0300

    add one example in java to compare with scala interpolation.

--- pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Strings.java
+++ pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Strings.java
@@ -4,6 +4,8 @@ public class Strings {
 
 	public static void main(String[] args) {
 
+		// Multi line strings
+
 		String str = "\"Java language\" redirects here. "
 				+ "\nFor the natural language from the Indonesian island of Java, see Javanese language."
 				+ "\nThis article is about a programming language. For the software package downloaded from java.com, "
@@ -11,7 +13,13 @@ public class Strings {
 
 		System.out.println(str);
 
+		// Strings interpolation
 
+		double discount = 50;
+
+		String message = "A discount of " + discount + "% has been applied";
+
+		System.out.println(message);
 
 	}
 }
