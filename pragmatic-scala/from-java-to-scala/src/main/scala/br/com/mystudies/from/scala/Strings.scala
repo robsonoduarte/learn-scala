e77ee5da4888e5b572be0cb232f7e31f341067d3
Robso Duarte <robson.o.d@gmail.com>
2016-08-02 21:10:52 -0300

    organize code in "blocks"

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Strings.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Strings.scala
@@ -1,57 +1,76 @@
 package br.com.mystudies.from.scala
 
-object String extends App{
+object String extends App {
 
+  def multiLine = {
 
-  var scala = """Scala is an acronym for “Scalable Language”.This means that Scala grows with you.
-               You can play with it by typing one-line expressions and observing the results.
-               But you can also rely on it for large mission critical systems, as many companies, including Twitter, LinkedIn, or Intel do."""
+    var scala = """Scala is an acronym for “Scalable Language”.This means that Scala grows with you.
+				  You can play with it by typing one-line expressions and observing the results.
+				  But you can also rely on it for large mission critical systems, as many companies, including Twitter, LinkedIn, or Intel do."""
 
-  println(scala)
+    println(scala)
 
+    // using the strigMargin combination with pipe
+    scala = """To some, Scala feels like a scripting language. Its syntax is concise and low ceremony;
+				  |its types get out of the way because the compiler can infer them.There’s a REPL and IDE worksheets for quick feedback.
+				  |Developers like it so much that Scala won the ScriptBowl contest at the 2012 JavaOne conference.""".stripMargin
 
+    println(scala)
+  }
 
-  // using the strigMargin combination with pipe
-  scala = """To some, Scala feels like a scripting language. Its syntax is concise and low ceremony;
-           |its types get out of the way because the compiler can infer them.There’s a REPL and IDE worksheets for quick feedback.
-           |Developers like it so much that Scala won the ScriptBowl contest at the 2012 JavaOne conference.""".stripMargin
+  def regex = {
+    // using to regex
+    var regex = """[S-s][C-c][A-a][L-l][A-a]"""
+    println("scala".matches(regex))
+    println("SCALA".matches(regex))
+    println("Scala".matches(regex))
+    println("Scalas".matches(regex))
+  }
 
-  println(scala)
+  
+  def interpolation = {
+    // Interpolation
+    val discount = 50.00
+    val message = s"A discount of $discount has been applied"
+    println(message)
+  }
 
+  
+  def complex = {
+    // using the curly brace for more complex expression and use the $ sign in string
+		val discount = 50.00
+    var price = 90
+    val totalPrice = s"The amount of discount is $$${price * discount / 100} dollars"
+    println(totalPrice)
 
+  }
 
-  // using to regex
-  var regex = """[S-s][C-c][A-a][L-l][A-a]"""
-
-
-  println("scala".matches(regex))
-  println("SCALA".matches(regex))
-  println("Scala".matches(regex))
-  println("Scalas".matches(regex))
-
-
-
-
-  // Interpolation
-
-  val discount = 50.00
-
-  val message = s"A discount of $discount has been applied"
-
-  println(message)
-
-
-
- // using the curly brace for more complex expression and use the $ sign in string
-
- var price = 90
- val totalPrice = s"The amount of discount is $$${price * discount / 100} dollars"
-
- println(totalPrice)
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  multiLine
+  regex
+  interpolation
+  complex
+  
+  
+  
 }
+
 
 
 
