df9ebdcc2c3a3b069e55acd818d9377dac07b5c6
Robso Duarte <robson.o.d@gmail.com>
2016-08-18 20:57:49 -0300

    add example about semicolon.

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/SemicolonIsOptional.scala
@@ -0,0 +1,16 @@
+package br.com.mystudies.from.scala
+
+
+object SemicolonIsOptional extends App {
+
+   // In Scala the semicolon are optional if the line don't have multiple statements or expressions.
+
+  // mult stat.
+   val str = "str"; println("print")
+
+   // one stat. per line
+   val x = 1
+   val y = 2
+
+
+}
