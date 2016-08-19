6a860a74d87011420daf2e2f3bba935b3f973936
Robso Duarte <robson.o.d@gmail.com>
2016-08-18 21:47:56 -0300

    add example how use block of code with semicolon.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/SemicolonIsOptional.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/SemicolonIsOptional.scala
@@ -13,4 +13,24 @@ object SemicolonIsOptional extends App {
    val y = 2
 
 
+
+   // but we need put the semicolon if start the new block of code
+
+   // here Scala started a new code block
+   val list1 = new java.util.ArrayList[Int];
+   {
+     println("Created list1")
+   }
+
+   // but here the Scala assumes we are creating an anonymous inner class that derives form ArraList[Int]
+   val list2 = new java.util.ArrayList[Int]
+   {
+     println("Created list2")
+   }
+
+
+   println(list1.getClass)
+   println(list2.getClass)
+
+
 }
