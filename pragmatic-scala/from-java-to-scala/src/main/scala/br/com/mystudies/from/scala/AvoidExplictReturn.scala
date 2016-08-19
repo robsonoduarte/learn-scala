7ccc2fb2535bfedb87ceef092cf79261cd2b8bc0
Robso Duarte <robson.o.d@gmail.com>
2016-08-18 21:56:58 -0300

    add example avoid explicit return.

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/AvoidExplictReturn.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.from.scala
+
+object AvoidExplictReturn extends App{
+
+	def check1 = true
+  def check2: Boolean = true
+  def check3: Boolean = return true
+
+  println(check1)
+  println(check2)
+  println(check3)
+
+}
