6b12a1d52c7ae07ba2fa7556042a0381213e547b
Robso Duarte <robson.o.d@gmail.com>
2016-08-23 21:04:21 -0300

    add example to customizing access modifiers.

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Access.scala
@@ -0,0 +1,22 @@
+package br.com.mystudies.from.scala
+
+object Access extends App {
+
+  val microwave = new Microwave
+
+  microwave.start
+  microwave.stop
+
+  //microwave.turnTable // don't compile
+}
+
+
+
+class Microwave{
+  // by default methods is plubic
+  def start = println("started")
+  def stop = println("stopped")
+
+  // to change access, explicitly the modifiers
+  private def turnTable  = println("turning table")
+}
