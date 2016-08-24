268403fcbdc32c0d04a03740638821fd6a9541e2
Robso Duarte <robson.o.d@gmail.com>
2016-08-24 11:15:47 -0300

    starting example about the modifier protected in scala.

---
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Protected.scala
@@ -0,0 +1,18 @@
+package br.com.mystudies.from.scala
+
+
+class Vehicle {
+  protected def checkEngine = {}
+}
+
+
+class Car extends Vehicle {
+  def start = checkEngine
+  def tow(car: Car) = car checkEngine
+  //def tow(vehicle: Vehicle) = vehicle checkEngine  -> in scala 
+}
+
+
+class GasStation {
+  //def fillGas(vehicle: Vehicle) = vehicle checkEngine
+}
