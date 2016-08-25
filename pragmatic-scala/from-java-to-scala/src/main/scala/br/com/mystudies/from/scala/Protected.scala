343117e8e8879eca48cfc7da307e6b06c0e6c9c9
Robso Duarte <robson.o.d@gmail.com>
2016-08-24 22:53:49 -0300

    put comment about protected acess in scala.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Protected.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/Protected.scala
@@ -9,10 +9,13 @@ class Vehicle {
 class Car extends Vehicle {
   def start = checkEngine
   def tow(car: Car) = car checkEngine
-  //def tow(vehicle: Vehicle) = vehicle checkEngine  -> in scala 
+  //def tow(vehicle: Vehicle) = vehicle checkEngine  -> don't compile
 }
 
 
 class GasStation {
-  //def fillGas(vehicle: Vehicle) = vehicle checkEngine
+  //def fillGas(vehicle: Vehicle) = vehicle checkEngine don't compile
 }
+
+
+// Scala is a lot more stringent about access to protected members
