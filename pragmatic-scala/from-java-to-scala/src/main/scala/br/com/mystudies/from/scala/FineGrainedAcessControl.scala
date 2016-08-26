c2fe04ec1a1bccbe7b9a801ff5f6ce5b824b51c5
Robso Duarte <robson.o.d@gmail.com>
2016-08-25 21:00:04 -0300

    add example about Fine-Grained Access Control.

--- pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/FineGrainedAcessControl.scala
+++ pragmatic-scala/from-java-to-scala/src/main/scala/br/com/mystudies/from/scala/FineGrainedAcessControl.scala
@@ -3,10 +3,39 @@ package br.com.mystudies.from.scala
 
 package society {
 
+    package professional{
+
+        class Executive{
+          private[professional] var workDetails = "workDetails"
+          private[society] var friends = "friends"
+          private[this] var secrets = "secrets"
+
+
+          def help(another: Executive) = {
+            println(another.workDetails)
+            println(another.friends)
+            //println(another.screts) don't compile
+          }
+        }
+
+        class Assistant {
+          def assist(anExcec: Executive)= {
+            println(anExcec.workDetails)
+            println(anExcec.friends)
+          }
+        }
+    }
+
+
+    package social {
+      class Acquaintance{
+         def socialize(person: professional.Executive) ={
+           println(person.friends)
+           // println(person.workDetails) don't compile
+         }
+      }
+    }
 }
 
 
 
-package social {
-
-}
