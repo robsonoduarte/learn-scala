934b13ca2f0b5243097d94e8009ced60e9325eee
Robson Duarte <robson.o.d@gmail.com>
2016-09-01 21:48:34 -0300

    example about construct expression executable statement and auxiliary constructor.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Construct.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Construct.scala
@@ -1,5 +1,35 @@
 package br.com.mystudies.scala
 
 class Construct(param:String) {
+  println(s"Creating an instance of Construct with parameter $param")
+}
+
+
+class Person(val firstName: String, val lastName: String){
+  var position: String = _
+  println(s"Creating ${toString}")
+
+  def this (firstName: String,lastName:String,positionHeld:String){
+    this(firstName,lastName)
+    position = positionHeld
+  }
+
+  override def toString = s"$firstName $lastName holds $position position"
+
+}
+
+
+
+object Run extends App{
+
+    println("Let's create an instance")
+    new Construct("sample")
+
+
+    var robson = new Person("Robson", "Duarte", "Programmer")
+    println(robson)
+
+    var ana = new Person("Ana", "Duarte")
+    println(ana)
 
 }
