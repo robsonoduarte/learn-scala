e9ed570c871a320ee10112e702122ea1029b4205
Robson Duarte <robson.o.d@gmail.com>
2016-09-14 21:58:26 -0300

    fix name.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/ParameterizedType.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/ParameterizedType.scala
@@ -1,6 +1,6 @@
 package br.com.mystudies.scala
 
-object ParamterizedType extends App {
+object ParameterizedType extends App {
 
 
   def echo[T](input1:T, input2:T) = println(s"got $input1 (${input1.getClass}) $input2 (${input2.getClass})")
