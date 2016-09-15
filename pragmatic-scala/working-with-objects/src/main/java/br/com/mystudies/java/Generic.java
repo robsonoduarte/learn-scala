9e861be92dd06c8c9f1316b03eea54fb088992cc
Robson Duarte <robson.o.d@gmail.com>
2016-09-15 10:35:19 -0300

    add one example of Generic in Java to compare with Parameterized Type in Scala.

---
+++ pragmatic-scala/working-with-objects/src/main/java/br/com/mystudies/java/Generic.java
@@ -0,0 +1,16 @@
+package br.com.mystudies.java;
+
+public class Generic {
+
+	public static void main(String[] args) {
+		echo("Robson", "Duarte");
+		echo(1, 5);
+	}
+
+
+	private static <T> void echo(T input1, T input2) {
+		System.out.println("got " + input1 + " " +  input1.getClass() + " " +  input2 + " " + input2.getClass());
+	}
+
+
+}
