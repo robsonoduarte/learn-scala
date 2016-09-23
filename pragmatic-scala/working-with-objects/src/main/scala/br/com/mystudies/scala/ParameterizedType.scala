0d1df5685df175c1fb2495e6657a1235f89017b8
R3WA <dev@r3wa.com.br>
2016-09-22 22:09:00 -0300

    add another example.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/ParameterizedType.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/ParameterizedType.scala
@@ -5,16 +5,16 @@ object ParameterizedType extends App {
 
   def echo[T](input1:T, input2:T) = println(s"got $input1 (${input1.getClass}) $input2 (${input2.getClass})")
 
+  // we can express that more clearly if our intention was to receive two different types of arguments.
+	def echo2[T1, T2](input1:T1, input2:T2) = println(s"got $input1 (${input1.getClass}) $input2 (${input2.getClass})")
+
 
 
   echo("Robson", "Ana")
   echo(3,5)
   echo(1, "Robson")
   //echo[Int](1,"Robson") error type mismatch -> the compiler will insist that both the arguments are of type int.
-
-
-
-
+  echo2(1, "Robson")
 
 }
 
