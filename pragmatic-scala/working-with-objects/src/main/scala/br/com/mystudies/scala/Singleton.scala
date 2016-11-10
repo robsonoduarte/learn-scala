752fa7fd9e2a073680ee415e551375431acf64b1
Robson Duarte <robson.o.d@gmail.com>
2016-11-10 21:44:16 -0200

    finished the example about Singleton.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Singleton.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Singleton.scala
@@ -1,5 +1,42 @@
 package br.com.mystudies.scala
 
-class Singleton {
+import scala.collection.Map
+
+
+
+
+
+object Singleton extends App{
+    println(MarkerFactory getMaker "red")
+    println(MarkerFactory getMaker "red")
+    println(MarkerFactory getMaker "blue")
+    println(MarkerFactory getMaker "blue")
+    println(MarkerFactory getMaker "green")
+}
+
+
+
+
+class Marker(color:String){
+
+  println(s"Creating --> ${this}")
+
+  override def toString = s"marker color $color"
+}
+
+
+
+
+
+object MarkerFactory {
+
+  private val markers = Map(
+          "red" -> new Marker("red"),
+          "blue" -> new Marker("blue"),
+          "yellow" -> new Marker("yellow")
+      )
+
+
+    def getMaker(color: String) = markers getOrElse(color, new Marker(color))
 
 }
