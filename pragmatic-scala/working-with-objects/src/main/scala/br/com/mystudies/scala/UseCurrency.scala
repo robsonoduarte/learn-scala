5b1a573f3485e9b6a28a1e8e98f2dd578ea868d8
Robson Duarte <robson.o.d@gmail.com>
2016-11-17 20:29:58 -0200

    add example how use the Enumeration

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/UseCurrency.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/UseCurrency.scala
@@ -1,5 +1,21 @@
 package br.com.mystudies.scala
 
+import br.com.mystudies.scala.currencies.Money
+
+
+
+
 object UseCurrency extends App {
 
+	import br.com.mystudies.scala.currencies.Currency._
+
+  val money = new Money(10, CNY)
+
+	println(money)
+
 }
+
+
+
+
+
