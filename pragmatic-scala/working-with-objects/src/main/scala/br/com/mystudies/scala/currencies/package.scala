ff80730e3d02f012f3559812415edf6de59701ef
Robson Duarte <robson.o.d@gmail.com>
2016-12-08 20:28:41 -0200

    add output of javap

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/currencies/package.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/currencies/package.scala
@@ -7,3 +7,13 @@ package object currencies {
   def convert(money: Money, to : Currency) = new Money(money.amount * 2, to)
 
 }
+
+
+
+
+/*
+  Compiled from "package.scala"
+  public final class br.com.mystudies.scala.currencies.package {
+  public static br.com.mystudies.scala.currencies.Money convert(br.com.mystudies.scala.currencies.Money, scala.Enumeration$Value);
+
+*/
