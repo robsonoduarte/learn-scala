88cbbd8e5f6dce40b662b74cd624a47d5563b3ec
Robson Duarte <robson.o.d@gmail.com>
2016-12-12 18:45:24 -0200

    format code.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Singleton.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/Singleton.scala
@@ -2,64 +2,46 @@ package br.com.mystudies.scala
 
 import scala.collection.Map
 
+object Singleton extends App {
 
+  println("#### MAKER BY MAKERFACTORY ####")
+  println(MarkerFactory getMaker "red")
+  println(MarkerFactory getMaker "red")
+  println(MarkerFactory getMaker "blue")
+  println(MarkerFactory getMaker "blue")
+  println(MarkerFactory getMaker "green")
+  println()
 
-
-
-object Singleton extends App{
-
-    println("#### MAKER BY MAKERFACTORY ####")
-    println(MarkerFactory getMaker "red")
-    println(MarkerFactory getMaker "red")
-    println(MarkerFactory getMaker "blue")
-    println(MarkerFactory getMaker "blue")
-    println(MarkerFactory getMaker "green")
-    println()
-
-    println("#### MAKER BY COMPANION OBJECT ####")
-    println(Marker getMaker "red")
-    println(Marker getMaker "red")
-    println(Marker getMaker "blue")
-    println(Marker getMaker "blue")
-    println(Marker getMaker "green")
+  println("#### MAKER BY COMPANION OBJECT ####")
+  println(Marker getMaker "red")
+  println(Marker getMaker "red")
+  println(Marker getMaker "blue")
+  println(Marker getMaker "blue")
+  println(Marker getMaker "green")
 
 }
 
-
-
-
-class Marker(color:String){
-
+class Marker(color: String) {
   println(s"Creating --> ${this}")
-
   override def toString = s"marker color $color"
 }
-
-
-
-
 
 object MarkerFactory {
 
   private val markers = Map(
-          "red" -> new Marker("red"),
-          "blue" -> new Marker("blue"),
-          "yellow" -> new Marker("yellow")
-      )
+    "red" -> new Marker("red"),
+    "blue" -> new Marker("blue"),
+    "yellow" -> new Marker("yellow"))
 
-    def getMaker(color: String) = markers getOrElse(color, new Marker(color))
+  def getMaker(color: String) = markers getOrElse (color, new Marker(color))
 }
-
 
 object Marker {
 
   private val markers = Map(
-          "red" -> new Marker("red"),
-          "blue" -> new Marker("blue"),
-          "yellow" -> new Marker("yellow")
-      )
+    "red" -> new Marker("red"),
+    "blue" -> new Marker("blue"),
+    "yellow" -> new Marker("yellow"))
 
-
-    def getMaker(color: String) = markers getOrElse(color, new Marker(color))
-
+  def getMaker(color: String) = markers getOrElse (color, new Marker(color))
 }
