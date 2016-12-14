fa765a26e523313b14f2c5e82399a58055272e7c
Robson Duarte <robson.o.d@gmail.com>
2016-12-14 20:05:34 -0200

    change the local o class.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/UseCar.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/UseCar.scala
@@ -1,21 +1,30 @@
 package br.com.mystudies.scala
 
-object UseCar extends App {
-
-   val car = new Car(2015)
-
-
-   println(s"Car made in year ${car.year}")
-   println(s"Miles driven ${car.miles}")
-   println(s"Drive for 10 miles")
-   car drive 10
-   println(s"Miles driven ${car.miles}")
+// see Car.java for comparison
+class Car(val year: Int) {
+  private var milesDriven: Int = 0
+  def miles = milesDriven
+  def drive(distance: Int) = milesDriven += Math.abs(distance)
 }
 
 
-// see Car.java to comparison
-class Car(val year: Int){
-  private var milesDriven : Int = 0
-  def miles = milesDriven
-  def drive(distance: Int) = milesDriven += Math.abs(distance)
+
+
+
+
+
+
+
+
+
+// USING THE CLASS
+object UseCar extends App {
+
+  val car = new Car(2015)
+
+  println(s"Car made in year ${car.year}")
+  println(s"Miles driven ${car.miles}")
+  println(s"Drive for 10 miles")
+  car drive 10
+  println(s"Miles driven ${car.miles}")
 }
