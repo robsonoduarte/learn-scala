8084f01955aec8658742aa57d50d54dd5e816cee
Robson Duarte <robson.o.d@gmail.com>
2016-12-13 22:20:56 -0200

    format the code.

--- pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/TypeAliasing.scala
+++ pragmatic-scala/working-with-objects/src/main/scala/br/com/mystudies/scala/TypeAliasing.scala
@@ -2,15 +2,11 @@ package br.com.mystudies.scala
 
 import scala.collection.mutable.Set
 
-
-
-
 object TypeAliasing extends App {
   type Cop = PoliceOfficer
 
   val topCop = new Cop("Robson")
   println(topCop.getClass)
 }
-
 
 class PoliceOfficer(val name: String)
