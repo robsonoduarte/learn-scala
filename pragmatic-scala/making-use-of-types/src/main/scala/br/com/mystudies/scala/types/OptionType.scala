b900eb0fafc0a8779e33fe53a90cea9341e984f7
Robson Duarte <robson.o.d@gmail.com>
2017-01-16 20:15:23 -0200

    add links of scala doc to Some and None.

--- pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/OptionType.scala
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/OptionType.scala
@@ -2,6 +2,20 @@ package br.com.mystudies.scala.types
 
 object OptionType extends App{
 
-  
-  
+
+  // Some and None is extends the Optinal
+  //http://www.scala-lang.org/api/current/scala/None$.html
+  //http://www.scala-lang.org/api/current/scala/Some.html
+
+  def commentOnPractice(input: String) = if( input == "test") Some("good") else None
+
+
+  Set("test", "hack") foreach( s => {
+    val commentDisplay = commentOnPractice(s).getOrElse("Found no comments")
+    println(s"input: $s comment: $commentDisplay")
+  })
+
+
+
+
 }
