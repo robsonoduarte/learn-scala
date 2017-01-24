9f12c1aa1527d37c00dc20dc49d974fe36054cac
Robson Duarte <robson.o.d@gmail.com>
2017-01-24 21:09:26 -0200

    add example about support of covariance.

---
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/SuportCovariance.scala
@@ -0,0 +1,20 @@
+package br.com.mystudies.scala.types
+
+object SuportCovariance extends App {
+
+  val dogs = Array(new Dog("Preto"), new Dog("Malhado"))
+
+	def workWithPets(pets: Array[Pet]) = { pets.foreach(println(_))}
+
+  //workWithPets(dogs) --> Compile error because the method accepts the array of Pets. But the method is benign and Scala doesn't know that.
+
+  // But we can tell to Scala that it's okay.
+  def playWitPets[T <: Pet](pets: Array[T]) = println(s"Play with pets: ${pets.mkString(", ")}")
+
+  playWitPets(dogs)
+
+
+}
+
+class Pet(val name:String){override def toString = name }
+class Dog(override val name: String) extends Pet(name)
