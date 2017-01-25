7871e465fe10d17e2c69cc6c78c47deb3c3ae1ab
Robson Duarte <robson.o.d@gmail.com>
2017-01-25 21:29:00 -0200

    add example about implicit functions.

---
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ImplicitFunctions.scala
@@ -0,0 +1,37 @@
+package br.com.mystudies.scala.types
+
+
+class DateHelper(offset:Int){
+
+	import scala.language.implicitConversions
+	import java.time.LocalDate._
+
+  def days(when: String) ={
+    val today = now
+    when match {
+      case "ago" => today.minusDays(offset)
+      case "fromNow" => today.plusDays(offset)
+      case _ => today
+    }
+  }
+}
+
+
+object DateHelper{
+  val ago = "ago"
+  val fromNow = "fromNow"
+  implicit def converInt2DateHelper(offset: Int) = new DateHelper(offset)
+}
+
+
+
+
+object DaysDSL extends App {
+
+	import DateHelper._
+
+   println(2 days ago)
+   println(2 days fromNow)
+   println(2 days "")
+
+}
