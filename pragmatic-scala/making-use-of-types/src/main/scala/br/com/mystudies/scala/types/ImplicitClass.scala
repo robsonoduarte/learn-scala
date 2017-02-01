ac93fd2e7ca49fb563d5db5936d2c9ab0b3ebfad
Robson Duarte <robson.o.d@gmail.com>
2017-02-01 21:01:17 -0200

    finished tha example about implicit class

--- pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ImplicitClass.scala
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ImplicitClass.scala
@@ -4,34 +4,31 @@ package br.com.mystudies.scala.types
 
 object UseDateUtil extends App {
 
-  import br.com.mystudies.scala.types.DateUtil._
+  import DateUtil._
 
-  print(2 days ago)
-  print(2 days fromNow)
-
+  println(2 days ago)
+  println(2 days fromNow)
 }
 
 
 
 object DateUtil{
 
-  import java.time.LocalDate._
+	import java.time.LocalDate._
 
   val ago = "ago"
   val fromNow = "fromNow"
 
   implicit class DateHelper(offset: Int){
-    def days(when:String){
+    def days(when:String) = {
      val today = now
       when match{
        case "ago" => today.minusDays(offset)
-       case "frowNow" => today.plusDays(offset)
+       case "fromNow" => today.plusDays(offset)
        case _ => today
      }
     }
   }
-
-
 }
 
 
