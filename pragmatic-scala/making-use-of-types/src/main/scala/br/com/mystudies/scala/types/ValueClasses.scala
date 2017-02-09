54c65b1e86df3878445faac3726f3fdf9eb4c382
Robson Duarte <robson.o.d@gmail.com>
2017-02-09 21:44:18 -0200

    add javap -c

--- pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ValueClasses.scala
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ValueClasses.scala
@@ -1,8 +1,53 @@
 package br.com.mystudies.scala.types
 
 object ValueClasses extends App {
-  
-  
-  
-  
+
+
+/*$ javap -c UseDateUtil.class
+Compiled from "ImplicitClass.scala"
+public final class br.com.mystudies.scala.types.UseDateUtil {
+  public static void main(java.lang.String[]);
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: aload_0
+       4: invokevirtual #18                 // Method br/com/mystudies/scala/types/UseDateUtil$.main:([Ljava/lang/String;)V
+       7: return
+
+  public static void delayedInit(scala.Function0<scala.runtime.BoxedUnit>);
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: aload_0
+       4: invokevirtual #22                 // Method br/com/mystudies/scala/types/UseDateUtil$.delayedInit:(Lscala/Function0;)V
+       7: return
+
+  public static java.lang.String[] args();
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: invokevirtual #26                 // Method br/com/mystudies/scala/types/UseDateUtil$.args:()[Ljava/lang/String;
+       6: areturn
+
+  public static void scala$App$_setter_$executionStart_$eq(long);
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: lload_0
+       4: invokevirtual #30                 // Method br/com/mystudies/scala/types/UseDateUtil$.scala$App$_setter_$executionStart_$eq:(J)V
+       7: return
+
+  public static long executionStart();
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: invokevirtual #34                 // Method br/com/mystudies/scala/types/UseDateUtil$.executionStart:()J
+       6: lreturn
+
+  public static void delayedEndpoint$br$com$mystudies$scala$types$UseDateUtil$1();
+    Code:
+       0: getstatic     #16                 // Field br/com/mystudies/scala/types/UseDateUtil$.MODULE$:Lbr/com/mystudies/scala/types/UseDateUtil$;
+       3: invokevirtual #38                 // Method br/com/mystudies/scala/types/UseDateUtil$.delayedEndpoint$br$com$mystudies$scala$types$UseDateUtil$1:()V
+       6: return
+
+
+}*/
+
+
+
 }
