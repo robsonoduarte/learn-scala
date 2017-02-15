23f8a5b77b5ca7e338de5338655cdba5255d6757
Robson Duarte <robson.o.d@gmail.com>
2017-02-15 21:11:26 -0200

    finished the example of type in Scala.

--- pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ImplicitConversions.scala
+++ pragmatic-scala/making-use-of-types/src/main/scala/br/com/mystudies/scala/types/ImplicitConversions.scala
@@ -2,6 +2,17 @@ package br.com.mystudies.scala.types
 
 object ImplicitConversions extends App {
 
+  import MyInterpolator._
+
+  val ssn = "123-45-6789"
+  val account = "0246781263"
+  val balance = "20145.23"
+
+
+  println(mask"""Acount: $account
+      |Social Securit Number : $ssn
+      |Balance: $$^${balance}
+      |Thanks for your business.""".stripMargin)
 
 }
 
@@ -12,9 +23,17 @@ object MyInterpolator{
   implicit class Interpolator(val context: StringContext) extends AnyVal{
 
     def mask(args: Any*) = {
+      val procesed = context.parts.zip(args).map{ item =>
+        val(text,expression) = item
 
+        if(text.endsWith("^"))
+            s"${text.split('^')(0)}$expression"
+         else
+           s"$text... ${expression.toString takeRight 4}"
+      }.mkString
+
+      new StringBuilder(procesed).append(context.parts.last)
     }
-
   }
 
 }
