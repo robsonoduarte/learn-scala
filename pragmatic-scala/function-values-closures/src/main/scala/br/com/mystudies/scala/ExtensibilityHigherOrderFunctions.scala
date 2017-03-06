98bb525b520f7a3b3b01ca1050d7a4f471228076
Robso Duarte <robson.o.d@gmail.com>
2017-03-06 20:14:05 -0300

    finished the first example.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ExtensibilityHigherOrderFunctions.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ExtensibilityHigherOrderFunctions.scala
@@ -5,8 +5,15 @@ object ExtensibilityHigherOrderFunctions extends App {
 
   def totalResultOverRange(number: Int, codeBlock: Int => Int) = {
 
+    var result = 0
+    for(i <- 1 to number)
+      result += codeBlock(i)
+      result
   }
 
+
+/*  println(totalResultOverRange(11, (i: Int) => i))*/
+  println(totalResultOverRange(11, i => i)) // Scala was able to infer that th type of parameter
 
 
 
