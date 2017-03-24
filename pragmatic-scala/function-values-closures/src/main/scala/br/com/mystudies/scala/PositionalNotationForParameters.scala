74d666242c6edac89225aae6b506c81b689014cc
Robso Duarte <robson.o.d@gmail.com>
2017-03-24 17:11:28 -0300

    add the more examples about use the underscore.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/PositionalNotationForParameters.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/PositionalNotationForParameters.scala
@@ -1,5 +1,13 @@
 package br.com.mystudies.scala
 
 object PositionalNotationForParameters extends App {
-  
+
+  val arr = Array(1,2,3,4,5)
+
+  val total1 = (0 /: arr){(x,y) => x + y}
+  val total2 = (0 /: arr){_+_}
+
+  val negativeNumberExists1 = arr exists {x => x < 0}
+  val negativeNumberExists2 = arr exists {_ < 0}
+
 }
