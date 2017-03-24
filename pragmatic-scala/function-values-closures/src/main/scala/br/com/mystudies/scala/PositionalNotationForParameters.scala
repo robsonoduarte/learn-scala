371633829cfa773ad4734b8045443a929534bd40
Robso Duarte <robson.o.d@gmail.com>
2017-03-24 17:12:35 -0300

    add println in example.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/PositionalNotationForParameters.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/PositionalNotationForParameters.scala
@@ -7,7 +7,16 @@ object PositionalNotationForParameters extends App {
   val total1 = (0 /: arr){(x,y) => x + y}
   val total2 = (0 /: arr){_+_}
 
+  println(total1)
+  println(total2)
+
+
   val negativeNumberExists1 = arr exists {x => x < 0}
   val negativeNumberExists2 = arr exists {_ < 0}
+
+
+  println(negativeNumberExists1)
+  println(negativeNumberExists2)
+
 
 }
