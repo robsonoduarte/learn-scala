f1414b70d30c10f3562713995e625c94b121c099
Robso Duarte <robson.o.d@gmail.com>
2017-03-28 20:22:07 -0300

    starting the example.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ParameterRouting.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ParameterRouting.scala
@@ -1,5 +1,18 @@
 package br.com.mystudies.scala
 
+import java.lang.Integer.MIN_VALUE
+
+
 object ParameterRouting extends App {
-  
+
+  var arr = Array(1,2,5,10,1,6)
+
+  val largest1 = ( MIN_VALUE /: arr )(Math.max(_,_))
+  print(largest1)
+
+  val largest2 = ( MIN_VALUE /: arr )(Math.max _)
+  print(largest2)
+
+
+
 }
