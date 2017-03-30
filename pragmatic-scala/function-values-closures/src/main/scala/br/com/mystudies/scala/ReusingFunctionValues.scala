934f993a20f726a1c6541065b7a3de4b452f9376
Robso Duarte <robson.o.d@gmail.com>
2017-03-30 20:48:41 -0300

    finished the reusing function values examples.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ReusingFunctionValues.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ReusingFunctionValues.scala
@@ -1,5 +1,42 @@
 package br.com.mystudies.scala
 
-object ReusingFunctionValues extends App {
-  
+object EquipmentUseNotDry extends App {
+   val equipment1 = new Equipment(input => {println(s"calc with $input");input})
+   val equipment2 = new Equipment(input => {println(s"calc with $input");input})
+
+   equipment1.simulate(4)
+   equipment2.simulate(6)
+}
+
+
+object EquipmentUseDry extends App {
+   val calculator = {input: Int => println(s"calc with $input"); input}
+
+   val equipment1 = new Equipment(calculator)
+   val equipment2 = new Equipment(calculator)
+
+   equipment1.simulate(4)
+   equipment2.simulate(6)
+}
+
+
+object EquipmentUseDry2 extends App {
+	def calculator(input: Int) = {println(s"calc with $input"); input}
+
+	val equipment1 = new Equipment(calculator)
+	val equipment2 = new Equipment(calculator)
+
+
+	equipment1.simulate(4)
+	equipment2.simulate(6)
+}
+
+
+
+
+class Equipment(val routime : Int => Int){
+  def simulate(input:Int)={
+    print("Running simulation...")
+    routime(input)
+  }
 }
