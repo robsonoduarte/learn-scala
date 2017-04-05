f73770881035087edd187e33e7caebcf63f22bcc
Robson Duarte <robson.o.d@gmail.com>
2017-04-05 20:58:32 -0300

    finished example of closure.

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/Closures.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/Closures.scala
@@ -1,8 +1,27 @@
 package br.com.mystudies.scala
 
 object Closures extends App {
-  
-  
-  
-  
+
+  def loopThrough(number:Int)(closure: Int => Unit) = for(i <- 1 to number) closure(i)
+
+
+  var result = 0
+  val addIt = {value:Int => result += value}
+
+
+
+  loopThrough(10)(element => addIt(element))
+  println(s"Total of values from 1 to 10 is $result")
+
+
+  result = 0
+
+  loopThrough(5)(addIt)
+  println(s"Total of values from 1 to 10 is $result")
+
+
+  var product = 1
+  loopThrough(5)(product *= _)
+  println(s"Product of values from 1 to 10 is $product")
+
 }
