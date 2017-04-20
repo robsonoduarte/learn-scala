eb277e1c3bd6b233131adf7a4c2208bc29c16bbd
Robson Duarte <robson.o.d@gmail.com>
2017-04-19 21:16:16 -0300

    add trait in example.

--- pragmatic-scala/traits/src/main/scala/br/com/mystudies/scala/UnderstandingTraits.scala
+++ pragmatic-scala/traits/src/main/scala/br/com/mystudies/scala/UnderstandingTraits.scala
@@ -3,9 +3,14 @@ package br.com.mystudies.scala
 object UnderstandingTraits extends App {
 }
 
-class Human(val name: String ){
-  def listen = println(s"Your  friend $name is listening")
+
+trait Friend{
+  val name: String
+  def listen = println(s"Your friend $name is listening")
 }
+
+
+class Human(val name: String ) extends Friend
 
 
 class Man(override val name: String ) extends Human(name)
