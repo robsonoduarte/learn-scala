205bea82b81b801faca9929f2dbe8219591a4f67
Robson Duarte <robson.o.d@gmail.com>
2017-05-04 20:46:25 -0300

    finished example of Selective Mixins

--- pragmatic-scala/traits/src/main/scala/br/com/mystudies/scala/SelectiveMixins.scala
+++ pragmatic-scala/traits/src/main/scala/br/com/mystudies/scala/SelectiveMixins.scala
@@ -1,5 +1,28 @@
 package br.com.mystudies.scala
 
 object SelectiveMixins extends App {
-  
+
+  def userFriend(friend: Friend) = friend.listen
+
+
+  val alf = new Cat("Alf")
+
+  //val friend: Friend = alf  ->>> compilation error cat is not one Friend
+  //userFriend(alf) ---> the same problem
+
+
+  // however Scala allows that we mix one instance with one trait
+
+  val angel = new Cat("Angel") with Friend
+  val friend : Friend = angel
+  angel.listen
+
+  userFriend(angel)
+
 }
+
+class Cat(val name: String) extends Animal
+
+
+
+
