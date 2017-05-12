bb8bc74169d1a8b7b954dbd6a983d02f79956eef
Robson Duarte <robson.o.d@gmail.com>
2017-05-12 17:54:04 -0300

    starting example about maps.

---
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/AssociativeMaps.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.scala
+
+object AssociativeMaps extends App{
+
+  val feeds = Map(
+        "Andy Hunt" -> "blog.tooshed.com",
+        "Dave Thomas" -> "pragdave.me",
+        "NFJS" -> "nofluffjuststuff.com/blog"
+      )
+
+
+
+}
