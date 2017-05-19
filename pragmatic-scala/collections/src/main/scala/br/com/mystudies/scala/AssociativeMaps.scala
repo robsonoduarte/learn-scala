1022b604b4a9a5a4afcf28f5edc6a34970ff1a56
Robson Duarte <robson.o.d@gmail.com>
2017-05-18 21:10:46 -0300

    Update AssociativeMaps.scala
    format code

--- pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/AssociativeMaps.scala
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/AssociativeMaps.scala
@@ -2,7 +2,7 @@ package br.com.mystudies.scala
 
 object AssociativeMaps extends App{
 
-  val feeds = Map(
+     val feeds = Map(
         "Andy Hunt" -> "blog.tooshed.com",
         "Dave Thomas" -> "pragdave.me",
         "NFJS" -> "nofluffjuststuff.com/blog"
@@ -15,7 +15,7 @@ object AssociativeMaps extends App{
 
 
      // filter on the values
-     val filterNameStartWithDAndPragInFeed = feeds filter {elem =>
+      val filterNameStartWithDAndPragInFeed = feeds filter {elem =>
         val (key, value) = elem
        (key startsWith "D") && (value contains "prag")
       }
@@ -31,30 +31,30 @@ object AssociativeMaps extends App{
 
 
 
-    // apply method to get
-    try{
-    	println(s"Get And's Feed Using Apply: ${feeds("Andy Hunt")}")
-    	print("Get Bill's Feed: ")
-    	println(feeds("Bill who"))
-    }
-    catch{
-      case ex: java.util.NoSuchElementException => println("Not Found")
-    }
+      // apply method to get
+      try{
+        println(s"Get And's Feed Using Apply: ${feeds("Andy Hunt")}")
+        print("Get Bill's Feed: ")
+        println(feeds("Bill who"))
+      }
+      catch{
+        case ex: java.util.NoSuchElementException => println("Not Found")
+      }
 
 
 
-    // updated
-    val newFeeds1 = feeds.updated("Robson Duarte", "https://twitter.com/robsonoduarte")
-    println(s"Robson's blog in original feeds: ${feeds.get("Robson Duarte")}")
-    println(s"Robson's blog in new feeds: ${newFeeds1.get("Robson Duarte")}")
+      // updated
+      val newFeeds1 = feeds.updated("Robson Duarte", "https://twitter.com/robsonoduarte")
+      println(s"Robson's blog in original feeds: ${feeds.get("Robson Duarte")}")
+      println(s"Robson's blog in new feeds: ${newFeeds1.get("Robson Duarte")}")
 
 
 
 
-    // update in mutable map
-    val mutableFeeds = scala.collection.mutable.Map(
-          "Scala Book Forum" -> "forums.pragprog.com/forum/87"
-        )
-    mutableFeeds("Groovy Book Forum") = "forums.pragprog.com/forum/246"
-    println(s"Number of forums: ${mutableFeeds.size}")
+      // update in mutable map
+      val mutableFeeds = scala.collection.mutable.Map(
+            "Scala Book Forum" -> "forums.pragprog.com/forum/87"
+          )
+      mutableFeeds("Groovy Book Forum") = "forums.pragprog.com/forum/246"
+      println(s"Number of forums: ${mutableFeeds.size}")
 }
