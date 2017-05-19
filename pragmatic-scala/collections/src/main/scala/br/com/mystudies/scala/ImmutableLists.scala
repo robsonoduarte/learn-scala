2286364915d99601f76b8a129cf01ac7f6d2b198
Robson Duarte <robson.o.d@gmail.com>
2017-05-19 18:04:23 -0300

    add example to prefix on element in list

--- pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/ImmutableLists.scala
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/ImmutableLists.scala
@@ -11,4 +11,12 @@ object ImmutableLists extends App {
   println(s"First feed: ${fedds.head}" )
   println(s"First feed: ${fedds(1)}" )
 
+  // prefix
+  val prefixedList = "forums.pragprog.com/forum/87" :: fedds
+  println(s"First Feed In Prefixed: ${prefixedList.head}")
+  println(s"The FeedPrefixed: ${prefixedList}")
+
+
+
+
 }
