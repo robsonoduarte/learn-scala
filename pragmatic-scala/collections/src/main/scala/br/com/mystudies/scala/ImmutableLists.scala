2ec0a0626db6c5a3596d8c5829555404962b0c3f
Robson Duarte <robson.o.d@gmail.com>
2017-05-23 19:45:24 -0300

    remane the val.

--- pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/ImmutableLists.scala
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/ImmutableLists.scala
@@ -3,28 +3,28 @@ package br.com.mystudies.scala
 object ImmutableLists extends App {
 
 
-  val fedds = List("blog.toolshed.com","pragdave.me","blog.agiledeveloper.com")
+  val feeds = List("blog.toolshed.com","pragdave.me","blog.agiledeveloper.com")
 
   // head and get
-  println(s"First feed: ${fedds.head}" )
-  println(s"First feed: ${fedds(1)}" )
+  println(s"First feed: ${feeds.head}" )
+  println(s"First feed: ${feeds(1)}" )
 
   // prefix
-  val prefixedList = "forums.pragprog.com/forum/87" :: fedds
+  val prefixedList = "forums.pragprog.com/forum/87" :: feeds
   println(s"First Feed In Prefixed: ${prefixedList.head}")
   println(s"The FeedPrefixed: ${prefixedList}")
 
 
   //append
   val feedsWithForums =
-    fedds ::: List("forums.pragprog.com/forums/87", "forums.pragprog.com/forum/287")
+    feeds ::: List("forums.pragprog.com/forums/87", "forums.pragprog.com/forum/287")
 
   println(s"First feed in feeds with forum: ${feedsWithForums.head}")
   println(s"Last feed in feeds with forum: ${feedsWithForums.last}")
 
 
   // append an element
-  val appendedList = fedds ::: List("agilelearner.com")
+  val appendedList = feeds ::: List("agilelearner.com")
   println(s"Last Feed in Appended: ${appendedList.last}" )
 
 
