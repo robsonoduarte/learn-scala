e611cacea77603307c4bafd3b30196b1ecadf7fc
Robson Duarte <robson.o.d@gmail.com>
2017-05-26 17:54:40 -0300

    add example of for using the yield.

--- pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/TheForExpression.scala
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/TheForExpression.scala
@@ -8,4 +8,11 @@ object TheForExpression extends App {
   // simple for
   for(i <- 1 to 3) {print("ho ")}
 
+  println()
+
+  // use yield
+  val result = for(i <- 1 to 10) yield i * 2
+  print(result)
+
+
 }
