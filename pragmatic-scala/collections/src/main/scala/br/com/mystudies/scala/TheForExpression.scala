4b30c7cecbe6d9e5177ae6d84b91f6b8e3cfa10a
Robson Duarte <robson.o.d@gmail.com>
2017-05-29 17:38:55 -0300

    Update TheForExpression.scala

--- pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/TheForExpression.scala
+++ pragmatic-scala/collections/src/main/scala/br/com/mystudies/scala/TheForExpression.scala
@@ -4,7 +4,6 @@ package br.com.mystudies.scala
 
 object TheForExpression extends App {
 
-
   // simple for
   for(i <- 1 to 3) {print("ho ")}
 
@@ -13,6 +12,5 @@ object TheForExpression extends App {
   // use yield
   val result = for(i <- 1 to 10) yield i * 2
   print(result)
-
 
 }
