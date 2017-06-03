a0241f9dace6fbd867d0c4b3856da20538c07a92
Robson Duarte <robson.o.d@gmail.com>
2017-06-02 22:08:50 -0300

    add explanatory text about the project.

--- pragmatic-scala/pattern-matching-regular-expressions/README.md
+++ pragmatic-scala/pattern-matching-regular-expressions/README.md
@@ -1,3 +1,3 @@
 Pattern Matching and Regular Expressions
 ---------------------------------
-**Readme in construction**
+In this project we will see Scala's mechanism form pattern matching, the case classes, and the extractors, as well as how to create and use regular expressions, the topics are:
--- pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingLiteralsAndConstants.scala
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingLiteralsAndConstants.scala
@@ -2,4 +2,20 @@ package br.com.mystudies.scala
 
 object MatchingLiteralsAndConstants extends App {
 
+
+  def activity(day: String){
+    day match {
+      case "Sunday" => println("Eat, sleep, repeat...")
+      case "Saturday" => println("Hang out with friends...")
+      case "Monday" => println("...code for fun...")
+      case "Friday" => println("...read a good book...")
+    }
+  }
+
+
+  List("Monday", "Sunday", "Saturday").foreach(activity)
+
 }
+
+
+
