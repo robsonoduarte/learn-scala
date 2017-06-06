d86bb9e78b17a75259f6045f2532b3143bd245a2
Robson Duarte <robson.o.d@gmail.com>
2017-06-06 19:38:27 -0300

    add example to matching one List.

--- pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingTuplesAndLists.scala
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingTuplesAndLists.scala
@@ -14,4 +14,21 @@ object MatchingTuplesAndLists extends App {
   processCoordinates("done")
 
 
+
+  def processItems(items: List[String]){
+    items match{
+      case List("apple", "ibm") => println("Apples and IBMs")
+      case List("red", "blue", "white") => println("Stars and Stripes...")
+      case List("red", "blue", _*) => println("colors red, blue,...")
+      case List("apple", "orange", otherFruits @ _*) => println(s"apples, oranges and $otherFruits")
+    }
+  }
+
+
+  processItems(List("apple", "ibm"))
+  processItems(List("red", "blue", "green"))
+  processItems(List("red", "blue", "white"))
+  processItems(List("apple", "orange", "grapes", "dates"))
+
+
 }
