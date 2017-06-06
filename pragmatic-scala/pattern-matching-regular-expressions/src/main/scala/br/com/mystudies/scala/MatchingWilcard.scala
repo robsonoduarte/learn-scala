9a661f212789570da0a713189fe79f044f85af34
Robson Duarte <robson.o.d@gmail.com>
2017-06-06 19:08:19 -0300

    finished example about matching a wildcard.

--- pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingWilcard.scala
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatchingWilcard.scala
@@ -1,8 +1,28 @@
 package br.com.mystudies.scala
 
+
 object MatchingWilcard extends App {
 
+	import br.com.mystudies.scala.DayOfWeek._
 
 
+  def activity(day: DayOfWeek.Value){
+     day match {
+      case SUNDAY => println("Eat, sleep, repeat...")
+      case SATURDAY => println("Drink beer with friends")
+      case _ => println("...code for fun...")
+    }
+  }
+
+
+	activity(SATURDAY)
+	activity(MONDAY)
 
 }
+
+
+object DayOfWeek extends Enumeration{
+  type DayOfWeek = Value
+  val SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY = Value
+}
+
