841d015e431eb21c5dc3ae26d3d508f0be5888c3
Robson Duarte <robson.o.d@gmail.com>
2017-06-07 21:21:25 -0300

    starting example about pattern variable and constants in case expressions.

---
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/PatternVariablesAndConstantsInCaseExpressions.scala
@@ -0,0 +1,28 @@
+package br.com.mystudies.scala
+
+object PatternVariablesAndConstantsInCaseExpressions extends App {
+
+  val sample = new Sample
+
+  try {
+    sample.process(0)
+  } catch {
+    case ex: Throwable => println(ex)
+  }
+
+  sample.process(100)
+
+}
+
+
+class Sample{
+  val max = 100
+
+  def process(input: Int){
+    input match{
+     // case max => println(s"You matched max $max") // in this case the Scala was inferred as a pattern variable and not as a constant value defined in the field
+     //case this.max => println(s"You matched max $max") // here we can refer to the hidden fields with explicit scoping
+      case `max`=> println(s"You matched max $max") // given a hint do Scala resolves the variable in the current scope.
+    }
+  }
+}
