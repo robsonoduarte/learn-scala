72b652c9a519a1e8331b5a2f1b87629362c2eb41
Robson Duarte <robson.o.d@gmail.com>
2017-06-09 20:18:39 -0300

    finished the example.

--- pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatcthingUsingExtractors.scala
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/MatcthingUsingExtractors.scala
@@ -1,8 +1,45 @@
 package br.com.mystudies.scala
 
-object MacthingUsingExtractors extends App {
+object MatcthingUsingExtractors extends App {
+
+  StockService process "GOOG"
+  StockService process "IBM"
+  StockService process "GOOG:310.84"
+  StockService process "GOOG:BUY"
+  StockService process "ERR:12.21"
+  StockService process "ERM"
 
 
+}
+
+object StockService{
+	def process(input: String){
+		input match{
+		case Symbol() => println(s"Look up price for valid symbol $input")
+		case ReceiveStockPrice(symbol @ Symbol(),price) => println(s"Received price $$$price for symbol $symbol")
+		case _ => println(s"Invalid input $input")
+		}
+	}
+}
 
 
+object Symbol{
+  def unapply(symbol:String) = symbol == "GOOG" || symbol == "IBM"
+}
+
+object ReceiveStockPrice{
+  def unapply(input: String) = {
+    try {
+    	if(input contains ":"){
+    		val splitQuote = input split ":"
+    				Some(splitQuote(0), splitQuote(1).toDouble)
+    	}
+    	else{
+    		None
+    	}
+    } catch {
+      case _ : NumberFormatException  => None
+    }
+
+  }
 }
