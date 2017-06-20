3e01744fbe02887be7068f433e5c6f2b1642e628
Robson Duarte <robson.o.d@gmail.com>
2017-06-20 19:41:44 -0300

    finished the example.

--- pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/RegularExpressionsAsExtractor.scala
+++ pragmatic-scala/pattern-matching-regular-expressions/src/main/scala/br/com/mystudies/scala/RegularExpressionsAsExtractor.scala
@@ -4,8 +4,22 @@ object RegularExpressionsAsExtractor extends App {
 
 
 
-  def process(input: String){
-  }
+  process("GOOG:310.84")
+  process("IBM:84.01")
+  process("GE:15.96")
 
+
+
+
+
+  def process(input: String){
+    val MatchSock = """^(.+):(\d*\.\d+)""".r
+    input match{
+      case MatchSock("GOOG", price) => println(s"We got GOOG at $$$price")
+      case MatchSock("GOOG", price) => println(s"IBM's trading at $$$price")
+      case MatchSock(symbol, price) => println(s"Price of $symbol $$$price")
+      case _ => println(s"not processing $input")
+    }
+  }
 
 }
