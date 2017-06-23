41b2a13b84669376c4a6bce3dede034e6a290ed5
Robson Duarte <robson.o.d@gmail.com>
2017-06-23 20:37:56 -0300

    add comments about fail optimization of Scala's compiler.

--- pragmatic-scala/programming-recursions/src/main/scala/br/com/mystudies/scala/TailCallOptimization.scala
+++ pragmatic-scala/programming-recursions/src/main/scala/br/com/mystudies/scala/TailCallOptimization.scala
@@ -3,6 +3,27 @@ package br.com.mystudies.scala
 object TailCallOptimization extends App {
 
 
+
+/*
+ *
+   @scala.annotation.tailrec
+    def factorial(number: Int) : BigInt = {
+      if(number == 0 ) 1
+      else number * factorial(number - 1)
+    }
+
+	  Since this version of the facotrial() is a regular recursion and not a tail recursion, the compiler will report an error
+  	because it can't optimized the recursion:
+
+    [error] TailCallOptimization.scala:11: could not optimize @tailrec annotated method factorial: it contains a recursive call not in tail position
+    [error]       else number * factorial(number - 1)
+    [error]                   ^
+    [error] one error found
+    [error] (compile:compileIncremental) Compilation failed
+    */
+
+
+
     @scala.annotation.tailrec
     def factorial(fact: BigInt, number: Int) : BigInt = {
       if(number == 0 ) fact
@@ -10,6 +31,6 @@ object TailCallOptimization extends App {
     }
 
 
-    println(factorial(1, 100000))
+    println(factorial(1, 10000))
 
 }
