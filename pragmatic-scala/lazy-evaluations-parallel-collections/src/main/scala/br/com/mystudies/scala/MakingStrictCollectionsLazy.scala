bc8c94190a317273ae062e92c4107489bae3ae26
Robson Duarte <robson.o.d@gmail.com>
2017-06-30 19:07:21 -0300

    format code.

--- pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/MakingStrictCollectionsLazy.scala
+++ pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/MakingStrictCollectionsLazy.scala
@@ -4,7 +4,6 @@ object MakingStrictCollectionsLazy extends App {
 
   val people = List(("Mark", 22), ("Bob",22),("Jane",8),("Jill",21), ("Nick",50), ("Nancy",42), ("Mike", 19), ("Sara", 12), ("Paula",42), ("Jhon",21))
 
-
   def isOlderThan17(person: (String,Int))={
     println(s"isOlderTha17 called for $person")
     val (_,age) = person
