590ef8faa2a7f7df2f71d10c721d5de9f5c4c8ed
Robson Duarte <robson.o.d@gmail.com>
2017-07-04 19:36:11 -0300

    add another example os use stream

--- pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/TheUltimatelyLazyStreams.scala
+++ pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/TheUltimatelyLazyStreams.scala
@@ -12,4 +12,25 @@ object TheUltimatelyLazyStreams extends App{
   println(generate(25).take(10).toList)
   println(generate(25).takeWhile(_ < 40).toList)
 
+
+  def isDivisibleBy(number: Int, divisor: Int) = number % divisor == 0
+
+  def isPrime(number: Int) = number > 1 && !(2 to number - 1).exists(isDivisibleBy(number, _))
+
+  def primes(starting: Int) : Stream[Int] = {
+    println(s"computing for $starting")
+    if(isPrime(starting))
+       starting #:: primes(starting + 1)
+    else
+      primes(starting + 1)
+  }
+
+
+  val primesFrom100 = primes(100)
+
+  println(primesFrom100.take(3).toList)
+  println("Let's ask for more")
+  println(primesFrom100.take(4).toList)
+
+
 }
