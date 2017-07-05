beb7f73550d42dd8f1c750400c6fd477caa4b308
Robson Duarte <robson.o.d@gmail.com>
2017-07-05 20:25:47 -0300

    add method to get weather data.

--- pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/ParallelCollections.scala
+++ pragmatic-scala/lazy-evaluations-parallel-collections/src/main/scala/br/com/mystudies/scala/ParallelCollections.scala
@@ -6,6 +6,22 @@ import scala.xml._
 object ParallelCollections extends App {
 
 
+  def getWeatherData(city: String ) = {
+    val url = "http://api.openweathermap.org/data/2.5/weather"
+
+    val response = Source.fromURL(s"$url?q=$city&units=imperial&mode=xml&APPID=863979fd70e0b430fa19554cc22118fc")
+    val xmlResponse = XML.loadString(response.mkString)
+
+    val cityName = (xmlResponse \\ "city" \ "@name").text
+    val temperature = (xmlResponse \\ "temperature" \ "@value").text
+    val condition = (xmlResponse \\ "weather" \ "@value").text
+
+    (cityName, temperature, condition)
+
+  }
+
+
+
 
 
 }
