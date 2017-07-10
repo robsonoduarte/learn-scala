91f331c7dc04ec8ec702ba6e6f6d36fe5cf77796
Robson Duarte <robson.o.d@gmail.com>
2017-07-10 17:21:28 -0300

    starting Actors in Scala.

--- pragmatic-scala/programming-with-actors/src/main/scala/br/com/mystudies/scala/CreatingActors.scala
+++ pragmatic-scala/programming-with-actors/src/main/scala/br/com/mystudies/scala/CreatingActors.scala
@@ -1,5 +1,19 @@
 package br.com.mystudies.scala
 
+import akka.actor.Actor
+
+
+
 object CreatingActors extends App {
-  
+
+
+}
+
+
+class HollywoodActor extends Actor {
+
+  def receive= {
+    ???
+  }
+
 }
