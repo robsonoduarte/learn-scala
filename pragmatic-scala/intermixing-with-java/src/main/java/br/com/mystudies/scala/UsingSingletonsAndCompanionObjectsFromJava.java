6c8ebdadf0327dd4350d39c002ad6a491930b315
Robson Duarte <robson.o.d@gmail.com>
2017-07-24 17:18:23 -0300

    add example how use the Scala's companion object in Java

--- pragmatic-scala/intermixing-with-java/src/main/java/br/com/mystudies/scala/UsingSingletonsAndCompanionObjectsFromJava.java
+++ pragmatic-scala/intermixing-with-java/src/main/java/br/com/mystudies/scala/UsingSingletonsAndCompanionObjectsFromJava.java
@@ -6,5 +6,9 @@ public class UsingSingletonsAndCompanionObjectsFromJava {
 
 		// using the singleton ( Scala's object )
 		Single.greet();
+
+		// using the the class and companion object
+		new Buddy().greet();
+		Buddy$.MODULE$.greet();
 	}
 }
