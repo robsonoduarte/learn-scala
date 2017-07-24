3bf8a3e5daf3b87b46233138dcd9c510d99a0253
Robson Duarte <robson.o.d@gmail.com>
2017-07-24 17:00:26 -0300

    add comment .

--- pragmatic-scala/intermixing-with-java/src/main/java/br/com/mystudies/scala/UsingSingletonsAndCompanionObjectsFromJava.java
+++ pragmatic-scala/intermixing-with-java/src/main/java/br/com/mystudies/scala/UsingSingletonsAndCompanionObjectsFromJava.java
@@ -3,6 +3,8 @@ package br.com.mystudies.scala;
 public class UsingSingletonsAndCompanionObjectsFromJava {
 
 	public static void main(String[] args) {
+
+		// using the singleton ( Scala's object )
 		Single.greet();
 	}
 }
