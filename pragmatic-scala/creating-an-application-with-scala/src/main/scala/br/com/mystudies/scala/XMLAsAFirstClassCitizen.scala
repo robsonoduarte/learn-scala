b9ff2b9d014b7d45b477b59513a0d42db91787ee
Robson Duarte <robson.o.d@gmail.com>
2017-07-27 19:43:01 -0300

    format code.

--- pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/XMLAsAFirstClassCitizen.scala
+++ pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/XMLAsAFirstClassCitizen.scala
@@ -5,9 +5,9 @@ object XMLAsAFirstClassCitizen extends App {
 
   val xmlFragment =
   <symbols>
-		<symbol ticker="AAPL"><units>200</units></symbol>
-		<symbol ticker="IBM"><units>215</units></symbol>
-	</symbols>
+	<symbol ticker="AAPL"><units>200</units></symbol>
+	<symbol ticker="IBM"><units>215</units></symbol>
+  </symbols>
 
 
   println(xmlFragment)
