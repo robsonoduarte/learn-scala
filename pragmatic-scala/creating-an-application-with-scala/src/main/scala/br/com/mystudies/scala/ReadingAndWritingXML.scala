38cacdf0f8de392b10eaa9226c7f80aaed54053c
Robson Duarte <robson.o.d@gmail.com>
2017-08-02 19:58:03 -0300

    add example how write on xml.

--- pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingXML.scala
+++ pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingXML.scala
@@ -5,12 +5,14 @@ import scala.xml.XML
 object ReadingAndWritingXML extends App {
 
 
+  // read xml
   val stocksAndUnits = XML load "stocks.xml"
 
   println(stocksAndUnits.getClass)
   println(s"File has ${(stocksAndUnits \\ "symbol").size} symbol elements")
 
 
+  // map the xml in new map(ticker -> units)
   val stocksAndUnitMap =
    ( Map[String,Int]() /: (stocksAndUnits \ "symbol" )) { (map,symbolNode) =>
      val ticker = (symbolNode \ "@ticker").toString()
@@ -21,6 +23,24 @@ object ReadingAndWritingXML extends App {
   println(stocksAndUnitMap)
   println(s"Number of symbol elements found is ${stocksAndUnitMap.size}")
 
+
+  // update and write the xml
+  val updatedStocksAndUnitsXML =
+    <symbol>
+			{stocksAndUnitMap map updateUnitsAndCreateXML}
+	  </symbol>
+
+
+
+  def updateUnitsAndCreateXML(element: (String, Int))= {
+    val (ticker,units) = element
+    <symbol ticker={ticker}>
+			<units>{units + 1}</units>
+		</symbol>
+  }
+
+
+  XML save("stock2.xml", updatedStocksAndUnitsXML )
 
 
 }
