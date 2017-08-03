1b404ed6a86662177f99ffe4888bc788857bda86
Robson Duarte <robson.o.d@gmail.com>
2017-08-03 16:04:50 -0300

    fix the problem of load xml.

--- pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingXML.scala
+++ pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingXML.scala
@@ -43,7 +43,7 @@ object ReadingAndWritingXML extends App {
   XML save("stock2.xml", updatedStocksAndUnitsXML )
 
 
-  val elementsCount = (XML.loadString("stock2.xml") \\ "symbol").size
+  val elementsCount = (XML.load("stock2.xml") \\ "symbol").size
   println(s"Saved file has ${elementsCount} symbol elements")
 
 
