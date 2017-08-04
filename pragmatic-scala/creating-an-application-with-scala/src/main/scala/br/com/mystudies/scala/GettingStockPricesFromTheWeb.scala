b3c61c6a165f613ae01b89cb568b6016154dfc6f
Robson Duarte <robson.o.d@gmail.com>
2017-08-04 18:30:38 -0300

    add stock loader.

--- pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/GettingStockPricesFromTheWeb.scala
+++ pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/GettingStockPricesFromTheWeb.scala
@@ -7,17 +7,20 @@ import java.time.LocalDate._
 import java.time.format.DateTimeFormatter._
 import java.util.Locale._
 import scala.io.Source
+import scala.xml.XML
 
 object GettingStockPricesFromTheWeb extends App {
 
   println(StockPriceFinder.getLatestClosingPrice("GOOG"))
+  println(StockLoader.load)
+
+
 
 }
 
 
 
 object StockPriceFinder{
-
   def getLatestClosingPrice(symbol: String) ={
     val url = s"http://www.google.com/finance/historical?q=NASDAQ:${symbol}&startdate=${yesterday}&output=csv"
     val data = Source.fromURL(url).mkString
@@ -25,9 +28,20 @@ object StockPriceFinder{
     closingPrice
   }
 
-
   private def yesterday = encode(now.minusDays(1).format(ofPattern("MMM d, yyyy",US)))
+}
 
+
+object StockLoader{
+  def load = {
+    val stocksAndUnits = XML.loadFile("stocks.xml")
+    ( Map[String,Int]() /: (stocksAndUnits \ "symbol") ){
+      (map, symbolNode) =>
+        val ticker = (symbolNode \ "@ticker").toString
+        val units = (symbolNode \ "units").text.toInt
+        map + (ticker -> units)
+    }
+  }
 }
 
 
