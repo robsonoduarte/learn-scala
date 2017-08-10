ff69a97c3986ec9a35cb9f3f7a554b7dd9798d55
Robson Duarte <robson.o.d@gmail.com>
2017-08-10 19:35:31 -0300

    add simple example using the scala test.

---
+++ pragmatic-scala/unit-testing/src/main/scala/br/com/mystudies/scala/UsingScalaTest.scala
@@ -0,0 +1,28 @@
+package br.com.mystudies.scala
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import java.util.ArrayList
+
+class UsingScalaTest extends FlatSpec with Matchers{
+
+
+  trait EmpytArrayList{
+    val list = new ArrayList[String]
+  }
+
+
+  "a list" should "be empty on create" in new EmpytArrayList{
+    list.size should be(0)
+  }
+
+
+  "a list" should "increase in size upon add" in new EmpytArrayList{
+    list add "Milk"
+    list add "Sugar"
+
+    list.size should be(2)
+  }
+
+
+}
