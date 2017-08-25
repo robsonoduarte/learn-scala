4ec6aaae52629acc6c3924bdd44e2c30a47b4462
Robson Duarte <robson.o.d@gmail.com>
2017-08-25 19:41:04 -0300

    add simple use of mockito in test

--- pragmatic-scala/unit-testing/src/main/scala/br/com/mystudies/scala/UsingMockito.scala
+++ pragmatic-scala/unit-testing/src/main/scala/br/com/mystudies/scala/UsingMockito.scala
@@ -2,10 +2,21 @@ package br.com.mystudies.scala
 
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import org.scalatest.mockito.MockitoSugar
 
-class UsingMockito extends FlatSpec with Matchers {
 
-  def withWordScorer(test: WordScore => Unit) = test(new WordScore)
+class UsingMockito extends FlatSpec with Matchers with MockitoSugar{
+
+  def withWordScorer(test: WordScore => Unit) =  {
+    val speelChecker = mock[SpellChecker]
+    when(speelChecker.isCorrect(anyString)).thenReturn(true)
+
+    test(new WordScore(speelChecker))
+
+    verify(speelChecker, times(1)).isCorrect(anyString)
+  }
 
 
   "score" should "return 0 for an empty word" in {
@@ -27,9 +38,10 @@ class UsingMockito extends FlatSpec with Matchers {
 }
 
 
-class WordScore(/*spellChecker: SpellChecker*/){
+class WordScore(spellChecker: SpellChecker){
   private val VOWELS = List('a', 'e', 'i', 'o', 'u')
   def score(word: String) ={
+		 spellChecker.isCorrect(word)
     (0 /: word){ (total,letter) => total + (if(VOWELS.contains(letter)) 1 else 2)}
 
     // or ->  word.foldLeft(0)((total,letter) => total + (if(VOWELS.contains(letter)) 1 else 2) )
