ca593ab025e23280aeb125476cda849db430d9d3
Robson Duarte <robson.o.d@gmail.com>
2017-09-15 13:13:16 -0300

    simplify the function.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Scala.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Scala.scala
@@ -3,7 +3,7 @@ package br.com.mystudies.scala
 object Scala {
 
 	def containsUpperCase(s: String) = {
-	 	s.toCharArray.exists( (c: Char) => c.isUpper )
+	 	s.toCharArray.exists( _.isUpper )
 	}
 
 
