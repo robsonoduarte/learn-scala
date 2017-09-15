20bdbe22faff808dd502a461f6dde78b5345a7e2
Robson Duarte <robson.o.d@gmail.com>
2017-09-15 16:00:59 -0300

    add examples of conditional expression.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/ConditionalExpressions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/ConditionalExpressions.scala
@@ -2,9 +2,17 @@ package br.com.mystudies.scala
 
 object ConditionalExpressions extends App {
 
+   // return one type of value ( Int )
+   def ce1(x: Int) = if (x>0) 1 else -1
 
-   def conditional(x: Int) = if (x>0) 1 else -1
+   // mixed-type expression
+	 def ce2(x: Int) = if (x>0) "positive" else -1
 
+
+
+	 println(ce1(-1).getClass())
+	 println(ce2(1).getClass())
+	 println(ce2(-1).getClass())
 
 
 
