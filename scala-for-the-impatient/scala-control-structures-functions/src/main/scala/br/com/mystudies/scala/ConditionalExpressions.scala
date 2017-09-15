9f9be40447b076285f2cc2060dbcc9b3da6062b3
Robson Duarte <robson.o.d@gmail.com>
2017-09-15 16:13:02 -0300

    finished the example.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/ConditionalExpressions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/ConditionalExpressions.scala
@@ -8,12 +8,19 @@ object ConditionalExpressions extends App {
    // mixed-type expression
 	 def ce2(x: Int) = if (x>0) "positive" else -1
 
+	 // else part is omitted
+	 def ce3(x: Int) = if (x>0) 1
+
+	 // equivalent a condition above
+	 def ce4(x: Int) = if (x>0) 1 else ()
+
 
 
 	 println(ce1(-1).getClass())
 	 println(ce2(1).getClass())
 	 println(ce2(-1).getClass())
-
+	 println(ce3(-1).getClass())
+	 println(ce4(-1).getClass())
 
 
 }
