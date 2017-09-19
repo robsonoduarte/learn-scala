8247040f31401f5ff16bf85c1e1ceca1d7b04655
Robson Duarte <robson.o.d@gmail.com>
2017-09-19 16:23:33 -0300

    add another example.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/StatementTermination.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/StatementTermination.scala
@@ -5,14 +5,16 @@ package br.com.mystudies.scala
 object StatementTermination extends App {
 
 
-  // the semicolon is optional
+  // the semicolon and curly braces is optional when have only one statement in line
   def plus(x: Int, y:Int) = x + y
 
+  // but when have more  than one statement it is not optional.
+  def plusOne(x: Int, y: Int) = { val z = x + y; z + 1}
 
 
 
 
-
-  print(plus(2,2))
+  println(plus(2,2))
+  println(plusOne(2,2))
 
 }
