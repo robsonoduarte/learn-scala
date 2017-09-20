73fcfdbe3cd93cbb28cbc8a8f1138a439eed224a
Robson Duarte <robson.o.d@gmail.com>
2017-09-20 20:46:04 -0300

    add one example

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/BlockExpressionsAndAssignments.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/BlockExpressionsAndAssignments.scala
@@ -3,9 +3,13 @@ package br.com.mystudies.scala
 object BlockExpressionsAndAssignments extends App {
 
 
-  
+  // the value of the block is the value of last expression
+  def foo1() = { val x = 10; val y = 10; x * y}
 
 
 
+
+
+  println(foo1)
 
 }
