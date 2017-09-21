663c6fe77a42742ea3495a687379472bd03f5a24
Robson Duarte <robson.o.d@gmail.com>
2017-09-21 19:47:55 -0300

    add while example.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
@@ -1,5 +1,17 @@
 package br.com.mystudies.scala
 
 object Loops extends App {
-  
+
+
+  // while loop
+  var a = 0
+
+  while(a < 10 ){
+    println(s"Value of a: ${a}")
+    a += 1
+  }
+
+
+
+
 }
