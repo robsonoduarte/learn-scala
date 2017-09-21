cbb54dc07837b901058b62a91f074cce33124825
Robson Duarte <robson.o.d@gmail.com>
2017-09-20 21:30:13 -0300

    fix the comment.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/BlockExpressionsAndAssignments.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/BlockExpressionsAndAssignments.scala
@@ -6,7 +6,7 @@ object BlockExpressionsAndAssignments extends App {
   // the value of the block is the value of last expression
   def foo1() = { val x = 10; val y = 10; x * y}
 
-  // when assignments have no value the of expression is Unit( equivalente of void in Java )
+  // when assignments have no value the expression is Unit( equivalent of void in Java )
   def foo2() = { var x = 10; x += 10}
 
 
