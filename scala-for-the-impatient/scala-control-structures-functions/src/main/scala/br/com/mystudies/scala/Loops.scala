aa242dd9b773702e53d5ba6456b5dabdc4d185d0
Robson Duarte <robson.o.d@gmail.com>
2017-09-21 19:51:36 -0300

    add example how use for.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
@@ -5,13 +5,15 @@ object Loops extends App {
 
   // while loop
   var a = 0
-
-  while(a < 10 ){
+  while(a <= 10 ){
     println(s"Value of a: ${a}")
     a += 1
   }
 
 
+ // scala don't have  for( initialize; test; update ) but you can use:
+  for(i <- 0 to 10 )
+    println(s"Value of i: ${i}")
 
 
 }
