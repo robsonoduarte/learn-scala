ba266156ffd76f08ae2c4ab8f644939c9f810e4a
Robson Duarte <robson.o.d@gmail.com>
2017-09-21 19:25:29 -0300

    add example how read line.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/InputAndOutput.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/InputAndOutput.scala
@@ -1,5 +1,7 @@
 package br.com.mystudies.scala
 
+import scala.io.StdIn._
+
 object InputAndOutput extends App {
 
   // for simple print use print or println function
@@ -8,5 +10,14 @@ object InputAndOutput extends App {
 
   // for print with format use printf
   printf("Hello, %s! You are %d years old.\n", "Fred", 42)
+
+
+  // read one line
+  val name = readLine("Your Name: ")
+  print("You age: ")
+  val age = readInt()
+  printf("Hello, %s! Next year, you will be %d.\n", name, age + 1)
+
+
 
 }
