acaf4b919bcd0b2cfb925313d81157fceb41c0a3
Robson Duarte <robson.o.d@gmail.com>
2017-09-22 18:02:37 -0300

    add example of how use the multiple generators

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/AdvancedForLoopsAndForComprehensions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/AdvancedForLoopsAndForComprehensions.scala
@@ -4,4 +4,17 @@ package br.com.mystudies.scala
 
 object AdvancedForLoopsAndForComprehensions extends App {
 
+  // this threaded for:
+  for(i <- 1 to 3)
+    for(j <- 1 to 3)
+      print( s"${(10 * i + j)} " )
+
+  println()
+
+  // can do with multiple generators on the for
+  for(i <- 1 to 3; j <- 1 to 3)
+    print( s"${(10 * i + j)} " )
+
+
+
 }
