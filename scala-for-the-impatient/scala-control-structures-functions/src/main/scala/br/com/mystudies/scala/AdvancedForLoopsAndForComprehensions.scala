14612aece1012325882fb0ce8bdbab161550cbab
Robson Duarte <robson.o.d@gmail.com>
2017-09-22 18:37:57 -0300

    format code.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/AdvancedForLoopsAndForComprehensions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/AdvancedForLoopsAndForComprehensions.scala
@@ -8,8 +8,7 @@ object AdvancedForLoopsAndForComprehensions extends App {
   for(i <- 1 to 3)
     for(j <- 1 to 3)
       print( s"${(10 * i + j)} " )
-
-  println()
+      println()
 
   // can do with multiple generators on the for
   for(i <- 1 to 3; j <- 1 to 3)
