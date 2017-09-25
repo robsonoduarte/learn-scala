34fa44116860c28e40e5e2a863feaa50be3a6785
Robson Duarte <robson.o.d@gmail.com>
2017-09-25 16:33:50 -0300

    format code.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Loops.scala
@@ -2,7 +2,6 @@ package br.com.mystudies.scala
 
 object Loops extends App {
 
-
   // while loop
   var a = 0
   while(a <= 10 ){
@@ -10,20 +9,16 @@ object Loops extends App {
     a += 1
   }
 
-
  // scala don't have  for( initialize; test; update ) but you can use:
   for(i <- 0 to 10 )
     println(s"Value of i: ${i}")
-
 
   // if you don't using the 1 to n ( inclusive ) you can use:
    for(x <- 0 until 10 )
      println(s"Value of x: ${x}")
 
 
-
   // for in String
   for( c <- "Hello")
     print(c)
-
 }
