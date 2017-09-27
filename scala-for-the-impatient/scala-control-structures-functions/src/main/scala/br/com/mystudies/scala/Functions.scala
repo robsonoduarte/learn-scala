bf072453d933be804216c0c8acd293d5e363c738
Robson Duarte <robson.o.d@gmail.com>
2017-09-27 19:13:21 -0300

    add example of function.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Functions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Functions.scala
@@ -2,6 +2,11 @@ package br.com.mystudies.scala
 
 object Functions extends App {
 
+  // to define one function, specify name, parameters and body:
+  def abs(x: Double ) = if(x >= 0 ) x else -x
 
+
+  println(abs(4))
+  println(abs(-4))
 
 }
