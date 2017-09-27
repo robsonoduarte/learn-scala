341263ed5694e2238e37c344c75b38fdf55fb71a
Robson Duarte <robson.o.d@gmail.com>
2017-09-27 20:43:41 -0300

    add example how considered an argument sequence.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
@@ -11,5 +11,11 @@ object VariableArguments extends App {
   }
 
 
+   // call the function with as many arguments:
+   println(sum( 1, 4, 9, 16 , 24 ))
+
+
+   // tell to compiler to be consider an argument sequence
+   println(sum( 1 to 5: _*))
 
 }
