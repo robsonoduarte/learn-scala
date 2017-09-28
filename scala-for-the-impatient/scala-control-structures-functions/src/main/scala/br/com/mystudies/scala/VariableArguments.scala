48d77c49a5b8a089d2b1a8b27af66b838759ead2
Robson Duarte <robson.o.d@gmail.com>
2017-09-28 18:14:58 -0300

    add new example.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
@@ -10,12 +10,14 @@ object VariableArguments extends App {
     result
   }
 
-
    // call the function with as many arguments:
    println(sum( 1, 4, 9, 16 , 24 ))
 
-
    // tell to compiler to be consider an argument sequence
    println(sum( 1 to 5: _*))
+
+
+
+
 
 }
