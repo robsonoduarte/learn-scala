785995ed589032e4f7f968572f7ead152aac9be9
Robson Duarte <robson.o.d@gmail.com>
2017-10-03 17:00:57 -0300

    edit code.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/VariableArguments.scala
@@ -2,7 +2,6 @@ package br.com.mystudies.scala
 
 object VariableArguments extends App {
 
-
   // take a variable number of arguments
   def sum(args: Int*) = {
     var result = 0
@@ -22,6 +21,4 @@ object VariableArguments extends App {
      if(args.length == 0) 0
      else args.head + recursiveSum(args.tail : _*)
    }
-
-
 }
