5991b87a63ab3d14c98517004e7281f95ac13725
Robson Duarte <robson.o.d@gmail.com>
2017-10-03 17:03:09 -0300

    edit code.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Functions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Functions.scala
@@ -5,7 +5,6 @@ object Functions extends App {
   // to define one function, specify name, parameters and body:
   def abs(x: Double ) = if(x >= 0 ) x else -x
 
-
   // using o block when function requires more than one expression
   def fac1(n: Int) = {
     var r = 1
@@ -17,11 +16,8 @@ object Functions extends App {
 
   def fac2(n: Int): Int = if(n <= 0) 1 else n * fac2(n -1)
 
-
-
   println(abs(4))
   println(abs(-4))
   println(fac1(4))
   println(fac2(4))
-
 }
