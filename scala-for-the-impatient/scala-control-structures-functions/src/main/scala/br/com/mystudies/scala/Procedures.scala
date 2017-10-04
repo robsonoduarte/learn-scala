cb56a7a924f5346c08ce3a8c31c9820bc977e498
Robson Duarte <robson.o.d@gmail.com>
2017-10-04 19:04:45 -0300

    fix English comment.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Procedures.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Procedures.scala
@@ -3,7 +3,7 @@ package br.com.mystudies.scala
 object Procedures extends App {
 
 
-  // A procedure returns no value, and only call it for tis side effect
+  // A procedure returns no value, and only call it for its side effect
   def box(s: String){
      val border = "-" * s.length + "--\n"
      println(s"${border}|${s}|\n${border}")
