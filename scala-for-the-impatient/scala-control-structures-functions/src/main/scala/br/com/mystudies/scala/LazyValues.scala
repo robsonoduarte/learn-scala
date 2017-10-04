fe9f0dff137f0c6675308eeb6ecbbb3862302ff6
Robson Duarte <robson.o.d@gmail.com>
2017-10-04 19:18:36 -0300

    add example about lazy values.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/LazyValues.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/LazyValues.scala
@@ -1,5 +1,13 @@
 package br.com.mystudies.scala
 
+import scala.io.Source
+
 object LazyValues extends App {
-  
+
+  // the val words will be evaluated when acessed for the first time
+  lazy val words = Source.fromFile("/file/not/found").mkString
+
+
+  // uncomment the print to see the lazy evaluated ( will throw one java.io.FileNotFoundException)
+  //print(words)
 }
