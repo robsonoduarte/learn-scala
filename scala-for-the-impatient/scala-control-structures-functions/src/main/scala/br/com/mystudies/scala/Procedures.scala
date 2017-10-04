1325ac964e39598ea5a4dcd244185c32f49371f0
Robson Duarte <robson.o.d@gmail.com>
2017-10-04 18:58:38 -0300

    add example of procedure in scala.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Procedures.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Procedures.scala
@@ -1,5 +1,15 @@
 package br.com.mystudies.scala
 
 object Procedures extends App {
-  
+
+
+  // A procedure returns no value, and only call it for tis side effect
+  def box(s: String){
+     val border = "-" * s.length + "--\n"
+     println(s"${border}|${s}|\n${border}")
+   }
+
+
+  box("Hello")
+
 }
