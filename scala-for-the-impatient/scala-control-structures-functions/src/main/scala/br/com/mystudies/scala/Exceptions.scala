700a2d6a9e1b34ede58646a8f8ecdcb5ddbf3d96
Robson Duarte <robson.o.d@gmail.com>
2017-10-05 20:21:42 -0300

    add example how throws one exception in Scala.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Exceptions.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/Exceptions.scala
@@ -2,4 +2,20 @@ package br.com.mystudies.scala
 
 object Exceptions extends App {
 
+
+  // like java we can throw one Exception
+  def sqrt(x: Int) = {
+    if(x>=0) Math.sqrt(x)
+    else throw new Exception("x should not be negative")
+  }
+
+
+
+  // but in Scala has no checked exceptions
+  println(sqrt(4))
+  println(sqrt(-4))
+
+
+
+
 }
