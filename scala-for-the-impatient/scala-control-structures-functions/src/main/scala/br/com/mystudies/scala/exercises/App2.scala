ffbf36b4c5f64cd351b08239f809698e5df212ec
Robson Duarte <robson.o.d@gmail.com>
2017-10-05 20:46:48 -0300

    move class and add new package to exercises.

---
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/App2.scala
@@ -0,0 +1,32 @@
+package br.com.mystudies.scala.exercises
+
+
+
+object App2 {
+
+  def countdown(n: Int) { for (x <- 0 to n reverse) print(x + " ") }
+
+  def product(string: String) {
+
+
+
+    for (x <- string ) yield x toChar
+
+
+    }
+
+
+
+
+    def main(args : Array[String]) {
+
+
+
+ //      countdown(20)
+
+       product("Hello")
+
+
+    }
+
+}
