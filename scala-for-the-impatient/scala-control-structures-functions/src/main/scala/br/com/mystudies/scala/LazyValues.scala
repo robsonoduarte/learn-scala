55f8e4ae3ebada2f5cbe3a7f9f471cb2da82417e
Robson Duarte <robson.o.d@gmail.com>
2017-10-06 17:34:47 -0300

    format code.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/LazyValues.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/LazyValues.scala
@@ -7,7 +7,6 @@ object LazyValues extends App {
   // the val words will be evaluated when acessed for the first time
   lazy val words = Source.fromFile("/file/not/found").mkString
 
-
   // uncomment the print to see the lazy evaluated ( will throw one java.io.FileNotFoundException)
   //print(words)
 }
