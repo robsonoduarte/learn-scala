ac52ca7d9dd34b1184f8dd8f0818b3b9dfcacbab
Robson Duarte <robson.o.d@gmail.com>
2017-11-14 17:31:47 -0200

    format code.

--- pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Strings.java
+++ pragmatic-scala/from-java-to-scala/scr/main/java/br/com/mystudies/from/java/Strings.java
@@ -5,7 +5,6 @@ public class Strings {
 	public static void main(String[] args) {
 
 		// Multi line strings
-
 		String str = "\"Java language\" redirects here. "
 				+ "\nFor the natural language from the Indonesian island of Java, see Javanese language."
 				+ "\nThis article is about a programming language. For the software package downloaded from java.com, "
@@ -20,6 +19,5 @@ public class Strings {
 		String message = "A discount of " + discount + "% has been applied";
 
 		System.out.println(message);
-
 	}
 }
