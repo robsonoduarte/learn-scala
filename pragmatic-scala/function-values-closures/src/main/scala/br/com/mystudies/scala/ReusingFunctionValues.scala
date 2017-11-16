e4f43ecc3716a2ee9aa66c0580dcb14c1e37d624
Robson Duarte <robson.o.d@gmail.com>
2017-11-16 16:41:13 -0200

    format code

--- pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ReusingFunctionValues.scala
+++ pragmatic-scala/function-values-closures/src/main/scala/br/com/mystudies/scala/ReusingFunctionValues.scala
@@ -21,14 +21,14 @@ object EquipmentUseDry extends App {
 
 
 object EquipmentUseDry2 extends App {
-	def calculator(input: Int) = {println(s"calc with $input"); input}
+   def calculator(input: Int) = {println(s"calc with $input"); input}
+	
+   val equipment1 = new Equipment(calculator)
+   val equipment2 = new Equipment(calculator)
 
-	val equipment1 = new Equipment(calculator)
-	val equipment2 = new Equipment(calculator)
 
-
-	equipment1.simulate(4)
-	equipment2.simulate(6)
+   equipment1.simulate(4)
+   equipment2.simulate(6)
 }
 
 
