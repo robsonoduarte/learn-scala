69515250f25c375e80ef5ecefdc2ec92b56ac4a8
Robso Duarte <robson.o.d@gmail.com>
2017-11-28 16:52:34 -0200

    finish the ex 2 and 3

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
@@ -2,4 +2,9 @@ package br.com.mystudies.scala.exercises
 
 object Ex2 extends App {
 
+   def unit() = {}
+
+   println(unit)
+   println(unit.getClass)
+
 }
--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -1,5 +1,11 @@
 package br.com.mystudies.scala.exercises
 
 object Ex3 extends App {
-  
+
+  var y = 0
+
+  val x = y = 1
+
+  println(y)
+  println(x)
 }
