e4233d9e0df54c7314a0b13b69546b0e0d4913ae
Robson Duarte <robson.o.d@gmail.com>
2017-11-28 17:08:32 -0200

    add comment about ex.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -1,5 +1,8 @@
 package br.com.mystudies.scala.exercises
 
+/**
+ * Come up with one situation where the assignment x = y = 1 is valid in Scala. (Hint: Pick a suitable type for x .)
+ */
 object Ex3 extends App {
 
   var y = 0
