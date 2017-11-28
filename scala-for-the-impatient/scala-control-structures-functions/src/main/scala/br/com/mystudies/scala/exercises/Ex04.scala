e6552872e4f46918cc263888538f9f2d7b329f87
Robso Duarte <robson.o.d@gmail.com>
2017-11-28 17:01:52 -0200

    finished the ex 04

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex04.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex04.scala
@@ -1,5 +1,15 @@
 package br.com.mystudies.scala.exercises
 
+/**
+ * Write a Scala equivalent for the Java loop
+ * for (int i = 10; i >= 0; i--) System.out.println(i);
+ */
 object Ex04 extends App {
+
+  // with a range and reverse
+  (0 to 10).reverse.foreach(println)
+
+  // with a for loop
+  for ( i <- 10.to(0, -1)) println(i)
 
 }
