bd2e5be9abc422207bbe8a006b14635a47afcd28
Robson Duarte <robson.o.d@gmail.com>
2017-11-28 17:08:02 -0200

    add comments about.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
@@ -1,10 +1,16 @@
 package br.com.mystudies.scala.exercises
 
+/**
+ * What is the value of an empty block expression {} ? What is its type?
+ */
 object Ex2 extends App {
 
    def unit() = {}
 
+   //Unit has only one value which is ()
    println(unit)
-   println(unit.getClass)
+
+    //His type is Unit
+   println(unit.isInstanceOf[Unit])
 
 }
