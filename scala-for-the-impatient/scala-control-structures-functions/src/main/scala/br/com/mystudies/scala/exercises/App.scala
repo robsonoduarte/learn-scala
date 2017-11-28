a80242eaa3cda4fefec3f4c297305b2bbd409ce2
Robson Duarte <robson.o.d@gmail.com>
2017-11-28 17:02:29 -0200

    move ex 04.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/App.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/App.scala
@@ -5,12 +5,10 @@ package br.com.mystudies.scala.exercises
 object App {
 
 
-  def loop() = {for(i <- 1 to 10){print(i)} }
 
   def countdown(n: Int) { for (x <- 0 to n reverse) print(x + " ") }
 
   def main(args : Array[String]) {
-    loop()
     countdown(30)
   }
 
