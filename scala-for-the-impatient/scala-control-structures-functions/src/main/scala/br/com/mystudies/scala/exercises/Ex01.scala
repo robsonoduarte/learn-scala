89de1d5bd906f0ec64aa14f64dc4d858b3015378
Robson Duarte <robson.o.d@gmail.com>
2017-11-28 17:05:37 -0200

    add comment about ex.

--- scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
@@ -1,6 +1,9 @@
 package br.com.mystudies.scala.exercises
 
-// Exercise 1 of chapter 2
+/**
+ * The signum of a number is 1 if the number is positive, –1 if it is negative, and 0 if it is zero.
+ * Write a function that computes this value.
+ */
 object Ex1 extends App {
 
   def signum( n: Int ) = if (n > 0) 1 else if (n < 0) -1 else 0
