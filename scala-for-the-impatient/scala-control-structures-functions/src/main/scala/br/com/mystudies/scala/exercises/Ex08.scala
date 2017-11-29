ea72516e5fd2a2ee71b16435368d7806ccfdb969
R3WA <dev@r3wa.com.br>
2017-11-29 10:39:11 -0200

    ex 08

---
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
@@ -0,0 +1,12 @@
+package br.com.mystudies.scala.exercises
+
+/**
+ * Write a function product(s : String) that computes the product, as described in the preceding exercises.
+ */
+object Ex08 extends App {
+
+  def product(s: String ) = s.map(_.toLong).product
+
+  println( product("Hello") == 9415087488L)
+
+}
