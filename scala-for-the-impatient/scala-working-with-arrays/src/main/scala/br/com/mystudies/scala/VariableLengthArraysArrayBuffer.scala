c154b4a3011263d87ec282114b24053fbd881bd7
Robson Duarte <robson.o.d@gmail.com>
2017-11-29 20:52:18 -0200

    finished the examples.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/VariableLengthArraysArrayBuffer.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/VariableLengthArraysArrayBuffer.scala
@@ -11,23 +11,34 @@ object VariableLengthArraysArrayBuffer extends App {
 
 
   // ADD ELEMENT
-  b += 1 //ArrayBuffer(1)
+  b += 1 // => ArrayBuffer(1)
 
   // ADD MULTIPLE ELEMENTS
-  b += (1,2,3,5) //ArrayBuffer(1, 1, 2, 3, 5)
+  b += (1,2,3,5) // => ArrayBuffer(1, 1, 2, 3, 5)
 
   // APPEND ANY COLLECTION
-  b++= Array(8, 13, 21) //ArrayBuffer(1, 1, 2, 3, 5, 8, 13, 21)
-  b++= List(30, 40) //ArrayBuffer(1, 1, 2, 3, 5, 8, 13, 21, 30, 40)
+  b++= Array(8, 13, 21) // => ArrayBuffer(1, 1, 2, 3, 5, 8, 13, 21)
+  b++= List(30, 40) // => ArrayBuffer(1, 1, 2, 3, 5, 8, 13, 21, 30, 40)
 
 
 
   //REMOVE THE LAST FIVE ELEMENTS
-  b.trimEnd(5) //ArrayBuffer(1, 1, 2, 3, 5)
+  b trimEnd 5 // => ArrayBuffer(1, 1, 2, 3, 5)
 
 
+  // INSERT BEFORE INDEX 2
+  b insert(2, 6) // => ArrayBuffer(1, 1, 6, 2, 3, 5)
+
+  // INSERT MANY ELEMENTS
+  b insert(2, 7, 8, 9) // => ArrayBuffer(1, 1, 7, 8, 9, 6, 2, 3, 5)
 
 
+  // REMOVE
+  b remove 2 // => ArrayBuffer(1, 1, 8, 9, 6, 2, 3, 5)
+
+
+  // REMOVE MANY ELEMENTS
+  b remove(2, 3) // => ArrayBuffer(1, 1, 2, 3, 5)
 
 
 
