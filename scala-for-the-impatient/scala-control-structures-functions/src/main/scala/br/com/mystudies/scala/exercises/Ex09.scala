df6c65660791d4d2d9889479d038324f94ee2881
Robson Duarte <robson.o.d@gmail.com>
2017-11-29 10:39:23 -0200

    ex 09

---
+++ scala-for-the-impatient/scala-control-structures-functions/src/main/scala/br/com/mystudies/scala/exercises/Ex09.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.scala.exercises
+
+/**
+ * Make the function of the preceding exercise a recursive function.
+ */
+object Ex09 extends App {
+
+  def product(s: String ): Long = if( s.tail != "") s.head.toLong * product(s.tail) else s.head.toLong
+
+
+  println( product("Hello") == 9415087488L )
+
+}
