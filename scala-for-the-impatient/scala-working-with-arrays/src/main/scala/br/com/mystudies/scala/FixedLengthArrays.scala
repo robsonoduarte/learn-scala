08aa366923ba4c5771ca8aeffec340b850419dd7
Robson Duarte <robson.o.d@gmail.com>
2017-11-29 20:09:24 -0200

    add example how create one integer array.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/FixedLengthArrays.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/FixedLengthArrays.scala
@@ -2,4 +2,17 @@ package br.com.mystudies.scala
 
 object FixedLengthArrays extends App {
 
+
+  // AN ARRAY OF TEN INTEGERS, ALL INITIALIZED WITH ZERO
+  val nums = new Array[Int](10)
+
+  nums(0) = 1
+  // nums(11) = 1 java.lang.ArrayIndexOutOfBoundsException: 11
+
+  println(nums mkString " ")
+
+
+
+
+
 }
