599b423de982e56173a072a28f2d9f08eba38b28
Robson Duarte <robson.o.d@gmail.com>
2017-11-30 20:09:11 -0200

    add example using the foreach.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TraversingArraysAndArrayBuffers.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TraversingArraysAndArrayBuffers.scala
@@ -6,13 +6,15 @@ object TraversingArraysAndArrayBuffers extends App {
 
 
   // visit elements directly
-  for( i <- a )
+  for(i <- a)
     println(i)
-
 
    // visit by index
   for( i <- 0 until a.length)
     println(a(i))
 
+
+  // use for each
+  a.foreach(println)
 
 }
