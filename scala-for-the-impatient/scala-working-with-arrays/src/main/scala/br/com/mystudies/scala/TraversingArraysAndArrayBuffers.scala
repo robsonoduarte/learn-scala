53b34a0c5056bfe4af4f42cde213a5a35c15b9a4
Robson Duarte <robson.o.d@gmail.com>
2017-11-30 19:53:51 -0200

    add example with for.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TraversingArraysAndArrayBuffers.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TraversingArraysAndArrayBuffers.scala
@@ -1,5 +1,12 @@
 package br.com.mystudies.scala
 
 object TraversingArraysAndArrayBuffers extends App {
-  
+
+  val a = Array(1,2,3,4,5,6)
+
+
+  // visit elements directly
+  for( i <- a )
+    println(i)
+
 }
