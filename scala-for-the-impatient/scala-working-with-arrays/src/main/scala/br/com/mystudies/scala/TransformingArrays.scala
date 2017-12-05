13e0f5c84ee4defdda059315917a63d8546c23fe
Robson Duarte <robson.o.d@gmail.com>
2017-12-05 16:48:58 -0200

    create a new array.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TransformingArrays.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/TransformingArrays.scala
@@ -6,7 +6,9 @@ object TransformingArrays extends App {
   val a = Array(2,3,5,7,11)
 
 
-  //
+  // use for(..) yield loop creates a new collection
+  var result = for( elem <- a ) yield 2 * elem
+  println( result mkString " " )
 
 
 
