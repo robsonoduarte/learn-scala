7aba5a74aa68a84f387b0e2cbb48bde35704da74
Robson Duarte <robson.o.d@gmail.com>
2017-12-06 17:35:33 -0200

    add example how sorting array.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/CommonsAlgorithms.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/CommonsAlgorithms.scala
@@ -1,5 +1,7 @@
 package br.com.mystudies.scala
 
+import scala.collection.mutable.ArrayBuffer
+
 object CommonsAlgorithms extends App {
 
   // sum elements
@@ -7,5 +9,17 @@ object CommonsAlgorithms extends App {
   val sum = Array( 1, 7, 2, 9).sum
   println(sum)
 
+
+  // get max element
+  val max = ArrayBuffer("Mary", "had", "a", "little", "lamb").max
+  println(max)
+
+
+  // sorts a array without modifying the original
+  val ab = ArrayBuffer(1,7,2,9)
+  val sorted = ab.sortWith(_ < _)
+
+  println(ab)
+  println(sorted)
 
 }
