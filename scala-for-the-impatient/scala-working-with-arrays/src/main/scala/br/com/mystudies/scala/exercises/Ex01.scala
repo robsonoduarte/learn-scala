f67ff3ea99e97ccf7d76907d85a3c71105a8bf07
Robson Duarte <robson.o.d@gmail.com>
2017-12-11 16:56:08 -0200

    start ex 1

---
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
@@ -0,0 +1,8 @@
+package br.com.mystudies.scala.exercises
+
+/**
+ * Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive).
+ */
+class Ex01 {
+
+}
