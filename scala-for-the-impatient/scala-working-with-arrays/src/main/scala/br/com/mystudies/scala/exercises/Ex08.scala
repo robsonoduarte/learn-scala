5eb4bcbc208636e5e103cd6fcefe0dddaeaa4fff
Robson Duarte <robson.o.d@gmail.com>
2017-12-15 11:55:11 -0200

    finished ex 08.

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
@@ -1,5 +1,15 @@
 package br.com.mystudies.scala.exercises
 
+
+/**
+ * Rewrite the example at the end of Section 3.4 , “ Transforming Arrays ,” on page 32 .
+ * Collect indexes of the negative elements, reverse the sequence, drop the last index,
+ * and call a.remove(i) for each index.
+ * Compare the efficiency of this approach with the two approaches in Section 3.4 .
+ */
 object Ex08 extends App {
-  
+
+  val a = Array(51,-42,30,-4,25,-51,8,42)
+  val res = for (i <- 0 until a.length if (i<=a.indexWhere(_<0)||a(i)>0)) yield a(i)
+
 }
