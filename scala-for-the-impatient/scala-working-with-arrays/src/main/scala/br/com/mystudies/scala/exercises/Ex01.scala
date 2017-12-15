4508dfea459a38f6952a80e78d39db6ffa0013bf
Robson Duarte <robson.o.d@gmail.com>
2017-12-15 10:13:41 -0200

    finished ex 01

--- scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
+++ scala-for-the-impatient/scala-working-with-arrays/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
@@ -1,8 +1,18 @@
 package br.com.mystudies.scala.exercises
 
+import scala.util.Random
+
 /**
  * Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive).
  */
-class Ex01 {
+object Ex01 extends App {
+
+  def randon(n:Int) = new Array[Int](n).map(_ => Random.nextInt(n))
+
+
+  val array = randon(100)
+
+
+  print(array.mkString(" "))
 
 }
