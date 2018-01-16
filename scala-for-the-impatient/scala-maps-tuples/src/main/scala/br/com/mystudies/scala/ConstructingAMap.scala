0e8e515d4848ae80b77ccb723023e80696aa9994
Robson Duarte <robson.o.d@gmail.com>
2018-01-16 21:05:54 -0200

    add example about mutable map.

--- scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/ConstructingAMap.scala
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/ConstructingAMap.scala
@@ -6,7 +6,14 @@ object ConstructingAMap extends App {
   // a immutable map ( defualt of Scala )
   val scores1 = Map( "Alice" -> 10, "Bob" -> 3, "Cindy" -> 8 )
 
-  print(scores1)
+  println(scores1)
+  println(scores1.getClass)
 
+
+  // a mutable map
+  val scores2 = collection.mutable.Map("Alice" -> 12, "Bob" -> 3, "Cindy" -> 8 )
+
+  println(scores2 )
+  println(scores2.getClass )
 
 }
