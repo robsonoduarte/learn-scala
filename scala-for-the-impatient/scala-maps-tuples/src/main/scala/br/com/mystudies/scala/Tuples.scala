690d3e8787fc978bdfe46c55e3926bd224e6368d
Robson Duarte <robson.o.d@gmail.com>
2018-01-23 20:21:41 -0200

    add example about Tuples

---
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/Tuples.scala
@@ -0,0 +1,34 @@
+package br.com.mystudies.scala
+
+object Tuples extends App {
+
+
+  // one Tuple3 of [Int, Double, java.lang.String]
+  val t = ( 1, 3.14, "Fred")
+
+  // access values
+  assert(t._2 == 3.14)
+
+  // use pattern match
+  val (first, second, third) = t
+
+  assert(first == 1)
+  assert(second == 3.14)
+  assert(third == "Fred")
+
+
+ // use a _ if don't need all components
+ val ( f, s, _) = t
+
+
+ // function can return o tuple
+
+ val t2 = "New York".partition(_.isUpper)
+ assert(t2._1 == "NY")
+ assert(t2._2 == "ew ork")
+
+
+
+
+
+}
