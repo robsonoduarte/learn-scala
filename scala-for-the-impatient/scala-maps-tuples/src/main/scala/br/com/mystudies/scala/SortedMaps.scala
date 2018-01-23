fb1607fff6c2b7b8da4aeaaae1293ca98a64190d
Robson Duarte <robson.o.d@gmail.com>
2018-01-23 19:29:29 -0200

    add example how use sorted map.

---
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/SortedMaps.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.scala
+
+object SortedMaps extends App {
+
+
+  // by default Scala gives a has table
+  val scores = collection.immutable.SortedMap("Alice" -> 10, "Fred" -> 7, "Bob" -> 3, "Cindy" -> 8)
+  print(scores)
+
+
+
+
+}
