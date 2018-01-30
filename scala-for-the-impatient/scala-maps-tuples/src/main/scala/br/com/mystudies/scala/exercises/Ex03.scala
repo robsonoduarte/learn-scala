92c5efe1286f345be2cdfa6d7897ada104fbd7f7
Robson Duarte <robson.o.d@gmail.com>
2018-01-30 19:23:06 -0200

    starting ex 3

---
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -0,0 +1,27 @@
+package br.com.mystudies.scala.exercises
+
+import scala.io.Source
+import scala.collection.immutable.HashMap
+
+/**
+ * Repeat the preceding exercise with an immutable map.
+ */
+object Ex03 extends App {
+
+  /*val words = new HashMap[String, Int]
+
+  Source.fromFile("src/main/resources/words.txt").getLines()
+    .foreach(word => {
+     words(word) = words.getOrElse(word, 0) + 1
+  })
+
+
+  assert(words("java") == 2)
+  assert(words("scala") == 2)
+  assert(words("perl") == 1)
+  assert(words("python") == 1)
+  assert(words("groovy") == 1)
+  assert(words("ruby") == 1)
+  assert(words("go") == 2)
+*/
+}
