a0d06e86d69c4ffd87d61b9238155ec6266e48f3
Robson Duarte <robson.o.d@gmail.com>
2018-02-20 19:44:50 -0300

    remove the ex 09.c

--- scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/exercises/App.scala
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/exercises/App.scala
@@ -12,27 +12,7 @@ object App {
 
 
 
-  def lteggt(values: Array[Int], v:Int) = {
-    var lt = 0
-    var eg = 0
-    var gt = 0
-
-    for(x <- values){
-
-      if (x > v) {
-       gt =  gt + 1
-      }
-      if (x < v) {
-       lt = lt + 1
-      }
-      if (x == v) {
-       eg = eg + 1
-      }
-    }
-
-    (lt, eg, gt)
-
-  }
+  
 
 
 
@@ -40,8 +20,6 @@ object App {
 
   def main(args : Array[String]) {
 
-
-  	  println(lteggt(Array(2, 1, 4, -10, 3, 5, 20, 6 , -15, -8, 8, 100),5))
 
   	   for(x <- "Hello".zip("World")){
   	     print(x)
