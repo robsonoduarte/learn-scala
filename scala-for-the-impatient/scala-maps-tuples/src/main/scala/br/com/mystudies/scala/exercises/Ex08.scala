865dd9358c78d6cc1c47c06796d3d4d35cf4e49a
Robson Duarte <robson.o.d@gmail.com>
2018-02-20 19:21:52 -0300

    put comment and rename argument and method.

--- scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
+++ scala-for-the-impatient/scala-maps-tuples/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
@@ -1,10 +1,14 @@
 package br.com.mystudies.scala.exercises
 
+/**
+ * Write a function minmax(values: Array[Int]) that returns a pair containing
+ * the smallest and largest values in the array.
+ */
 object Ex08 extends App {
   
   
-    def minMax(values: Array[Int]) = (values.min,values.max)
+    def minmax(a: Array[Int]) = (a.min,a.max)
   
   
-    assert(minMax(Array(10,2, 20, -1, 30)) == (-1,30) )  
+    assert(minmax(Array(10,2, 20, -1, 30)) == (-1,30) )  
 }
