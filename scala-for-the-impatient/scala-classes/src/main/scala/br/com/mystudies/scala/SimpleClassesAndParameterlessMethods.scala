a8e5df2091cfcfd26f31b63737e55bed4b2f6c53
Robson Duarte <robson.o.d@gmail.com>
2018-02-20 21:07:11 -0300

    finhised the example.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/SimpleClassesAndParameterlessMethods.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/SimpleClassesAndParameterlessMethods.scala
@@ -2,4 +2,29 @@ package br.com.mystudies.scala
 
 object SimpleClassesAndParameterlessMethods extends App {
   
+  // the usual way to use class
+  val myCounter = new Counter // or new Counter()
+  myCounter.increment()
+  println(myCounter.current)
+
+  
+  // call a parameterless method with our without parentheses
+  myCounter.current // Ok
+  myCounter.current() // Also Ok 
+  
+  
+  // but you can enforce the style without parentheses declaring current without ()  
+    myCounter.current_ // Ok
+    //myCounter.current_() //  no compile 
+  
+}
+
+
+
+// A class is not declared as public
+class Counter{ 
+  private var value = 0 // You must initialize the field
+  def increment() = value += 1 // Methods are public by default
+  def current() = value
+  def current_ = value
 }
