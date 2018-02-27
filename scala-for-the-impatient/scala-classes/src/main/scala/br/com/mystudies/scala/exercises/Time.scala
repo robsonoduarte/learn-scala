9fbb77734e12e77719c8074ed35c517c53a89054
Robson Duarte <robson.o.d@gmail.com>
2018-02-27 19:56:44 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Time.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Time.scala
@@ -5,5 +5,4 @@ class Time( val hours:Int, val minutes:Int, val seconds: Int = 0) {
 	def before(other: Time) = {
 		hours >= other.hours && minutes >= other.minutes && seconds > other.seconds
 	}
-
 }
