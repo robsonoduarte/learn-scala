a185ef3602d4f3566d8a1eba75f298c89cfdc0f6
Robson Duarte <robson.o.d@gmail.com>
2018-03-27 19:06:04 -0300

    format code

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Person.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Person.scala
@@ -1,3 +1,5 @@
 package br.com.mystudies.scala.exercises
 
-class Person(var name: String = "Some Person", var age: Int = 0) {}
+class Person(var name: String = "Some Person", var age: Int = 0) {
+  
+}
