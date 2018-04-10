35f0d6fb76a55948241a5731518b1d472f48830a
Robson Duarte <robson.o.d@gmail.com>
2018-04-10 20:34:04 -0300

    add example how user object private fields

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ObjectPrivateFields.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ObjectPrivateFields.scala
@@ -2,8 +2,12 @@ package br.com.mystudies.scala
 
 object ObjectPrivateFields extends App {
   
+  val counter = new Counter3
+  
+  
 }
 
 class Counter3{
-  
+  private[this] var value = 0 // only the methods of the class can access     
+  print(value)
 }
