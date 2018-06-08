bab8c55149f439ca810028050d2f2befee3e3cb1
Robson Duarte <robson.o.d@gmail.com>
2018-06-08 19:37:56 -0300

    finished example about Auxiliary Constructor.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/AuxiliaryConstructors.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/AuxiliaryConstructors.scala
@@ -2,4 +2,32 @@ package br.com.mystudies.scala
 
 object AuxiliaryConstructors extends App {
   
+  val p1 = new Person3
+  val p2 = new Person3("Robson")
+  val p3 = new Person3("Robson", 41 )
+  
+  println(p1)
+  println(p2)
+  println(p3)
+  
+}
+
+
+class Person3{
+  private var name = ""
+  private var age = 0
+  
+  def this(name: String){
+    this()
+    this.name = name
+    
+  }
+  
+  
+  def this(name: String, age: Int ){
+    this(name)
+    this.age = age
+  }
+  
+  
 }
