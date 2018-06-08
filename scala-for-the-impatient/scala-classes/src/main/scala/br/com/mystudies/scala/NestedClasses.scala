ed41b2846e43f01fbfa480cbbfe31120caa773a7
Robson Duarte <robson.o.d@gmail.com>
2018-06-08 20:22:41 -0300

    finished example.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/NestedClasses.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/NestedClasses.scala
@@ -4,10 +4,24 @@ import scala.collection.mutable.ArrayBuffer
 
 class NestedClasses {
   
+  val chatter = new Network
+  val myFace = new Network
+  
+  val fred = chatter.join("Fred")
+  val wilma = chatter.join("Wilma")
+  
+  fred.contacts += wilma
+  
+  val barney = myFace.join("Barney")
+  //fred.contacts += barney -> No can't add a myFace.Member to a buffer o chatter.Members elements
+  
 }
 
-
-
+/*
+ * In scala , we can nest just anything inside anything
+ * 
+ * like functions inside another functions and classes inside other class
+ */
 class Network{
   class Member(val name: String){
     val contacts = new ArrayBuffer[Member]
