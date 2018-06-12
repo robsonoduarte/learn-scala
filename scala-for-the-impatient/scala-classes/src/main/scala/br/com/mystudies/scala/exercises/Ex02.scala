08ce2f218f44bf6e58fc5681f3f9a3bfe68981df
Robson Duarte <robson.o.d@gmail.com>
2018-06-12 20:09:08 -0300

    Ex 02.

---
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
@@ -0,0 +1,32 @@
+package br.com.mystudies.scala.exercises
+
+/**
+ * Write a class BankAccount with methods deposit and withdraw, and a read-only property balance.
+ */
+object Ex02 extends App {
+     
+  class BankAccount(private var _balance:Double = 0.0) {
+
+  	def deposit(value:Double) = {
+  	  _balance += value
+  	}
+
+  	def withdrow(value: Int ) = {
+  	  if(_balance - value > 0 ){
+  	    _balance -= value		    	    
+  	  }  	  
+  	}
+
+	  def balance = _balance
+  }
+  
+  
+  
+  val myBA = new BankAccount
+  myBA.deposit(100)
+  myBA.withdrow(20)
+  
+  assert( myBA.balance == 80.0 )
+  
+  
+}
