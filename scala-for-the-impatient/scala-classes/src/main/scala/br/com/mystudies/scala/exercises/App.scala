cb1929fe675d67c823a76780acf34c48548989ee
Robson Duarte <robson.o.d@gmail.com>
2018-06-12 20:08:14 -0300

    refacotoring code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/App.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/App.scala
@@ -2,28 +2,15 @@ package br.com.mystudies.scala
 
 import br.com.mystudies.scala.exercises.Employee
 import br.com.mystudies.scala.exercises.Car
-import br.com.mystudies.scala.exercises.Person
 import br.com.mystudies.scala.exercises.Time
 import br.com.mystudies.scala.exercises.Student
-import br.com.mystudies.scala.exercises.BankAccount
 
 
 object App{
 
   def main(args : Array[String]) {
 
-	val bankAccount =  new BankAccount;
-
-	// one form to access bankAccount'methods
-    bankAccount deposit 10
-    bankAccount withdrow
-
-    // another form to access bankAccount'methods
-    bankAccount.deposit(10)
-    bankAccount.withdrow
-
-    println(bankAccount.total) // accessing a read-only properties in bankAccount
-
+	
 
     val time1 = new Time(20,21,24)
     val time2 = new Time(20,21,23)
