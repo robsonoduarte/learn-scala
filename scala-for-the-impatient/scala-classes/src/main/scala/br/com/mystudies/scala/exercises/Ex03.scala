7b0655f9a9e76b0464519531892d9dd91782ac66
Robson Duarte <robson.o.d@gmail.com>
2018-06-12 20:09:31 -0300

    Ex 03.

---
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -0,0 +1,13 @@
+package br.com.mystudies.scala.exercises
+
+object Ex03 extends App {
+  
+  
+  class Time( val hours:Int, val minutes:Int) {
+  	def before(other: Time) = {
+  		hours >= other.hours && minutes >= other.minutes
+  	}
+  }
+  
+  
+}
