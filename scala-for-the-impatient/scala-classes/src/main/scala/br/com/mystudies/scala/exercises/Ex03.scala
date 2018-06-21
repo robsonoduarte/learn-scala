657641eb27f3f8408513fc241dc82d924ace67b9
Robson Duarte <robson.o.d@gmail.com>
2018-06-21 20:42:04 -0300

    add comment in exercice.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -1,5 +1,11 @@
 package br.com.mystudies.scala.exercises
 
+/**
+ * Write a class Time with read-only properties hours and minutes and a method before(other: Time):
+ * Boolean that checks whether this time comes before the other.
+ *
+ * A Time object should be constructed as new Time(hrs, min), where hrs is in military time format (between 0 and 23).
+ */
 object Ex03 extends App {
   
   class Time( val hours:Int, val minutes:Int) {
