1cb1c476826f9f367019b2c15c8a3e92f7d1154a
Robson Duarte <robson.o.d@gmail.com>
2018-06-21 21:06:14 -0300

    finished ex 05.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex05.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex05.scala
@@ -1,5 +1,38 @@
 package br.com.mystudies.scala.exercises
 
+import scala.beans.BeanProperty
+
+/**
+ * Make a class Student with read-write JavaBeans properties name (of type String) and id (of type Long).
+ *
+ * What methods are generated? (Use javap to check.) Can you call the JavaBeans getters and setters in Scala?
+ *
+ * Should you?
+ */
+
 object Ex05 extends App {
+  
+  
+  class Student(@BeanProperty var name: String, @BeanProperty var id: Long)
+  
+  /* javap =>    
+    Scala generate the methods getName, setName, getId, setId    
+    public class br.com.mystudies.scala.exercises.Student {
+    public java.lang.String name();
+    public void name_$eq(java.lang.String);
+    public void setName(java.lang.String);
+    public int id();
+    public void id_$eq(int);
+    public void setId(int);
+    public java.lang.String getName();
+    public int getId();
+    public br.com.mystudies.scala.exercises.Student();
+		}    
+   */
+  
+  
+   val robson = new Student("Robson", 100)
+  
+   assert(robson.getId == robson.id)
   
 }
