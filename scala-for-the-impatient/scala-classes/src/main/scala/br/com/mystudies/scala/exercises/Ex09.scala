7782cf7cb289917ca81eb9bd80ea672b552b4dc1
robsonoduarte <robson.o.d@gmail.com>
2018-08-03 21:07:08 -0300

    ex 09

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex09.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex09.scala
@@ -4,6 +4,6 @@ package br.com.mystudies.scala.exercises
  * In the Person class of Section 5.1 , “Simple Classes and Parameterless Methods,” on page 49 ,
  * provide a primary constructor that turns negative ages to 0.
  */
-object Ex06 extends App {
+object Ex09 extends App {
 
 }
