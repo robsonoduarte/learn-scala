76c629d892a7f67e09382b13e060eef00e69d286
robsonoduarte <robson.o.d@gmail.com>
2018-08-15 21:40:08 -0300

    ex 07.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex07.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex07.scala
@@ -1,9 +1,23 @@
 package br.com.mystudies.scala.exercises
 
 /**
- * In the Person class of Section 5.1 , “Simple Classes and Parameterless Methods,” on page 49 ,
- * provide a primary constructor that turns negative ages to 0.
+ * Write a class Person with a primary constructor that accepts a string containing a first name,
+ * a space, and a last name, such as new Person("Fred Smith") . Supply read-only properties firstName and lastName .
+ *
+ * Should the primary constructor parameter be a var , a val , or a plain parameter? Why?
  */
 object Ex07 extends App {
+
+  class Person(name:String){
+    val (firstName, lastName) = name.split(" ") match{
+      case Array(x:String, y:String, _* ) => (x,y)
+      case _ => (null,null)
+    }
+  }
+
+  val person = new Person("Robson Duarte")
+
+  assert(person.firstName == "Robson" )
+  assert(person.lastName == "Duarte")
 
 }
