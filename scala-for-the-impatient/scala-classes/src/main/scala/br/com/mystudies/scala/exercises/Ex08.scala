58bacf175fdbe517ceaf074dfdaa9d17d6729a28
robsonoduarte <robson.o.d@gmail.com>
2018-08-15 21:40:19 -0300

    ex 08.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex08.scala
@@ -1,8 +1,12 @@
 package br.com.mystudies.scala.exercises
 
 /**
- * In the Person class of Section 5.1 , “Simple Classes and Parameterless Methods,” on page 49 ,
- * provide a primary constructor that turns negative ages to 0.
+ * Make a class Car with read-only properties for manufacturer, model name, and model year, and a read-write property
+ * for the license plate.
+ *
+ * Supply four constructors. All require the manufacturer and model name. Optionally, model year and license plate
+ * can also be specified in the constructor. If not, the model year is set to -1 and the license plate
+ * to the empty string. Which constructor? Why?
  */
 object Ex08 extends App {
 
