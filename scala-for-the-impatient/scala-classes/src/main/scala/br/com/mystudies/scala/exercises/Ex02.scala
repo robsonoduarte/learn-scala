5023a88925f087a627dbd2758dae90ad393819bc
robsonoduarte <robson.o.d@gmail.com>
2018-09-10 11:20:04 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex02.scala
@@ -4,7 +4,7 @@ package br.com.mystudies.scala.exercises
  * Write a class BankAccount with methods deposit and withdraw, and a read-only property balance.
  */
 object Ex02 extends App {
-     
+
   class BankAccount(private var _balance:Double = 0.0) {
 
   	def deposit(value:Double) = {
@@ -13,19 +13,18 @@ object Ex02 extends App {
 
   	def withdrow(value: Int ) = {
   	  if(_balance - value > 0 ){
-  	    _balance -= value		    	    
-  	  }  	  
+  	    _balance -= value
+  	  }
   	}
 
 	  def balance = _balance
   }
-  
-  
+
+
   val myBA = new BankAccount
   myBA.deposit(100)
   myBA.withdrow(20)
-  
+
   assert( myBA.balance == 80.0 )
-  
-  
+
 }
