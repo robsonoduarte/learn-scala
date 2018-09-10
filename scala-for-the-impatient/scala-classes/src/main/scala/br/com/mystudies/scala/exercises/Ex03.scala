67a547e2f2d8d5facbf7096aa07d7c1dc9ef9a48
robsonoduarte <robson.o.d@gmail.com>
2018-09-10 11:20:13 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Ex03.scala
@@ -7,19 +7,17 @@ package br.com.mystudies.scala.exercises
  * A Time object should be constructed as new Time(hrs, min), where hrs is in military time format (between 0 and 23).
  */
 object Ex03 extends App {
-  
+
 	class Time( val hours:Int, val minutes:Int) {
 		def before(other: Time) = {
 				hours < other.hours || ( hours == other.hours &&  minutes < other.minutes)
 		}
 	}
 
-	
+
   val t1 = new Time(20,50)
-  val t2 = new Time(20,49) 
-  
-  
-  
-  assert( t2.before(t1) == true) 
-  
+  val t2 = new Time(20,49)
+
+  assert( t2.before(t1) == true)
+
 }
