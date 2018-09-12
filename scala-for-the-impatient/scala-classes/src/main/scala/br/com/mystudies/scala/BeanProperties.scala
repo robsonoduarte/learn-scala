41ae5157268ce1f7dab4077d218b5a41d8092cfe
robsonoduarte <robson.o.d@gmail.com>
2018-09-12 14:32:34 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/BeanProperties.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/BeanProperties.scala
@@ -3,19 +3,18 @@ package br.com.mystudies.scala
 import scala.beans.BeanProperty
 
 object BeanProperties extends App {
-  
+
   var person = new Person
-  
+
   // Generate four methods
   person.name = "Robson"
   person.setName(person.name + " Duarte ")
-  
+
   println(person.name)
   println(person.getName())
-  
+
   class Person{
 	  @BeanProperty var name: String = _
   }
-  
-  
+
 }
