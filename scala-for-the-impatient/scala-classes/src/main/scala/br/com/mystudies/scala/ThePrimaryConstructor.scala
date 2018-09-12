afed0a4c4222c203ba00d38dbaa8be317afbbe7b
robsonoduarte <robson.o.d@gmail.com>
2018-09-12 14:33:22 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ThePrimaryConstructor.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ThePrimaryConstructor.scala
@@ -1,12 +1,11 @@
 package br.com.mystudies.scala
 
 object ThePrimaryConstructor extends App {
-  val p = new Person4("Robson Duarte", 41) 
-  println(p.description) 
+  val p = new Person4("Robson Duarte", 41)
+  println(p.description)
 }
 
-
-class Person4(private val name: String, private val age: Int ){ // the primary constructor 
+class Person4(private val name: String, private val age: Int ){ // the primary constructor
   // all statements in the class definition will executed with primary constructor
   println("Just constructed another person")
   def description = s"${name} is ${age} years old"
