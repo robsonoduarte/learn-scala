9d7c838447e939dfbea795f3bf90dc5819aca092
robsonoduarte <robson.o.d@gmail.com>
2018-09-28 13:34:10 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/PropertiesWithOnlyGetters.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/PropertiesWithOnlyGetters.scala
@@ -3,7 +3,6 @@ package br.com.mystudies.scala
 import java.util.Date
 
 object PropertiesWithOnlyGetters extends App {
-
   val message = new Message
 
   //message.timeStamp = new Date don't compile no setter method
