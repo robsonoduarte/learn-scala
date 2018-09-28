3a419de3e9277c9b9e4e3883cc75698d36759990
robsonoduarte <robson.o.d@gmail.com>
2018-09-28 13:33:40 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ObjectPrivateFields.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/ObjectPrivateFields.scala
@@ -2,7 +2,6 @@ package br.com.mystudies.scala
 
 object ObjectPrivateFields extends App {
   val counter = new Counter
-
   class Counter{
     private[this] var value = 0 // only the methods of the class can access
     print(value)
