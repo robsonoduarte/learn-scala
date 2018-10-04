3cd48f54b0898a72b607c05a16cabb11a092f778
robsonoduarte <robson.o.d@gmail.com>
2018-10-04 14:16:48 -0300

    format code.

--- scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Employee.scala
+++ scala-for-the-impatient/scala-classes/src/main/scala/br/com/mystudies/scala/exercises/Employee.scala
@@ -1,5 +1,3 @@
 package br.com.mystudies.scala.exercises
 
-class Employee(val name: String = "Robson Duarte", val salary: Double = 10.00){
-
-}
+class Employee(val name: String = "Robson Duarte", val salary: Double = 10.00){}
