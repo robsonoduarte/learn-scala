507ee8985b58169593ce5de9b8bd80a9d485fd49
robsonoduarte <robson.o.d@gmail.com>
2018-11-15 13:39:41 -0200

    remove codes of another project.

--- scala-for-the-impatient/scala-actors/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
+++ scala-for-the-impatient/scala-actors/src/main/scala/br/com/mystudies/scala/exercises/Ex01.scala
@@ -1,17 +1,7 @@
 package br.com.mystudies.scala.exercises
 
-import scala.util.Random
 
-/**
- * Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive).
- */
 object Ex01 extends App {
 
-  def randon(n:Int) = new Array[Int](n).map(_ => Random.nextInt(n))
 
-
-  val array = randon(100)
-
-
-  print(array.mkString(" "))
 }
