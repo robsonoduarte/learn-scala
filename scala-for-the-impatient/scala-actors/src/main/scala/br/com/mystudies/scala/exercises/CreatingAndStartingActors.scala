c61caa0744a546b4fbc74871fea1fdcec3643bc7
robsonoduarte <robson.o.d@gmail.com>
2018-11-15 14:21:50 -0200

    finished example how create a Scala Actor.

---
+++ scala-for-the-impatient/scala-actors/src/main/scala/br/com/mystudies/scala/exercises/CreatingAndStartingActors.scala
@@ -0,0 +1,33 @@
+package br.com.mystudies.scala.exercises
+
+import scala.actors.Actor
+import scala.actors.Actor._
+
+object CreatingAndStartingActors extends App {
+
+
+  // extending the Trait Actor to create on Actor
+  class HiActor extends Actor{
+    def act = {
+      while(true){
+        receive{
+          case "Hi" => println("Hello")
+        }
+      }
+    }
+  }
+
+  val actor1 = new HiActor
+  actor1.start
+
+
+  // Using the actor method of Actor companion object to create and start
+  val actor2 = actor{
+    while(true){
+      receive{
+        case "Hi" => println("Hello")
+      }
+    }
+  }
+
+}
