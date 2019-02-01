c9711b9826c55e3deadd17e834788e3953507f7a
robsonoduarte <robson.o.d@gmail.com>
2019-01-31 22:28:49 -0200

    start the project

---
+++ akka/akka-actors/README.md
@@ -0,0 +1,9 @@
+Creating an Application with Scala
+-----------------------
+In this project we will progressively build an application to find the new worth of investments in the stock market using the concepts that we saw in another projects of this repository, **the topics are**:
+
+* [Getting Users' Input](https://github.com/robsonoduarte/learn-scala/blob/master/pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/GettingUsersInput.scala)
+* [Reading and Writing Files](https://github.com/robsonoduarte/learn-scala/blob/master/pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingFiles.scala)
+* [XML as a First-Class Citizen](https://github.com/robsonoduarte/learn-scala/blob/master/pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/XMLAsAFirstClassCitizen.scala)
+* [Reading and Writing XML](https://github.com/robsonoduarte/learn-scala/blob/master/pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/ReadingAndWritingXML.scala)
+* [Getting Stock Prices From The Web ( the app )](https://github.com/robsonoduarte/learn-scala/blob/master/pragmatic-scala/creating-an-application-with-scala/src/main/scala/br/com/mystudies/scala/GettingStockPricesFromTheWeb.scala)
---
+++ akka/akka-actors/src/main/scala/br/com/mystudies/scala/XMLAsAFirstClassCitizen.scala
@@ -0,0 +1,45 @@
+package br.com.mystudies.scala
+
+object XMLAsAFirstClassCitizen extends App {
+
+  val xmlFragment =
+  <symbols>
+	<symbol ticker="AAPL"><units>200</units></symbol>
+	<symbol ticker="IBM"><units>215</units></symbol>
+  </symbols>
+
+
+  println(xmlFragment)
+  println(xmlFragment.getClass)
+
+  println("--------------------------------------------------------")
+
+  // using the Xpath-like query
+  val symbolsNodes =  xmlFragment \ "symbol"
+  symbolsNodes foreach println
+  println(symbolsNodes.getClass)
+
+  println("--------------------------------------------------------")
+
+  // search through all elements in the hierarchy
+  val unitNodes = xmlFragment \\ "units"
+  unitNodes foreach println
+  println(unitNodes.getClass)
+  println(unitNodes.head.text)
+
+  println("--------------------------------------------------------")
+
+  // using the pattern match
+  unitNodes.head match {
+    case <units>{numberOfUnits}</units> => println(s"Units: $numberOfUnits")
+  }
+
+  println("--------------------------------------------------------")
+
+  // grab all elements
+  xmlFragment  match {
+    case <symbols>{symbolNodes @ _*}</symbols> =>
+      for(symbolNode @ <symbol>{_*}</symbol> <- symbolsNodes)
+        println("%-7s %s".format(symbolNode \ "@ticker", (symbolNode \ "units").text))
+  }
+}
